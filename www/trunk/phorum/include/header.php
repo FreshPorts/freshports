c5e9e2e3ed4bce6525db7c4e4bf8f84edaaf2db5
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2000-05-14 07:40:54 +0000

    removed the styles Modified Files: 	header.php
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@105 46e82423-29d8-e211-989e-002590a4cdd4

--- www/trunk/phorum/include/header.php
+++ www/trunk/phorum/include/header.php
@@ -9,27 +9,17 @@ require( "/www/freshports.org/_private/getvalues.php3");
 <meta name="Phorum DB" content="<?PHP echo $DB->type; ?>">
 <meta name="PHP Version" content="<?PHP echo phpversion(); ?>">
 <TITLE>phorum - <?PHP if(isset($ForumName)) echo $ForumName; ?><?PHP echo $title; ?></TITLE>
-<STYLE> BODY {font-family: Arial, Helvetica;font-size: 10pt;}
-  TD {font-family: Arial, Helvetica;font-size: 10pt;}
-  INPUT {font-family: Arial, Helvetica;font-size: 10pt;}
-  TEXTAREA {font-family: Arial, Helvetica;font-size: 10pt;width: 500px;}
-  .forum_title {font-family: Arial, Helvetica;font-size: large;}
-  .forum {
- 	font-family: Arial, Helvetica;
- 	font-size: medium;
- }
-  .nav {font-family: MS Sans Serif,Geneva,sans-serif;font-size: 8pt;}</STYLE>
 </HEAD>
 <BODY BGCOLOR="#FFFFFF" LINK="#0000CC">
 <? include("/www/freshports.org/_private/header.inc") ?>
 <table width="100%" border="0">
 <tr><td colspan="2">
-<div class=forum_title><b>forum
+<font size="+2">forum
 <?PHP 
 if ($ForumName != '') {
    echo " - $ForumName";
 }
-?></b></div>
+?></b></font>
 </td></tr>
 <tr><td valign="top" width="100%">
 

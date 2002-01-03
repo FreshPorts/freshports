50846dfd6e5075a24f6120d791462177005a8477
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2002-01-03 03:04:18 +0000

    convert to postgresql PR 45
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@809 46e82423-29d8-e211-989e-002590a4cdd4

--- www/branches/FreshPorts2/search.php
+++ www/branches/FreshPorts2/search.php
@@ -1,17 +1,18 @@
 <?
-   # $Id: search.php,v 1.1.2.1 2002-01-02 02:53:50 dan Exp $
-   #
-   # Copyright (c) 1998-2001 DVL Software Limited
+	# $Id: search.php,v 1.1.2.2 2002-01-03 03:04:18 dan Exp $
+	#
+	# Copyright (c) 1998-2001 DVL Software Limited
 
-   require("./include/common.php");
-   require("./include/freshports.php");
-   require("./include/databaselogin.php");
-   require("./include/getvalues.php");
+	require("./include/common.php");
+	require("./include/freshports.php");
+	require("./include/databaselogin.php");
+	require("./include/getvalues.php");
 
+	require("../classes/ports.php");
 
-   freshports_Start("Search",
-               "freshports - new ports, applications",
-               "FreeBSD, index, applications, ports");
+	freshports_Start("Search",
+					"freshports - new ports, applications",
+					"FreeBSD, index, applications, ports");
 
 ?>
 <table width="100%">
@@ -37,30 +38,33 @@ if ($search) {
 
    echo "</td></tr>\n<tr><td>";
 
-$fp = fopen("/home/freshports.org/logs/searchlog.txt", "a");
+$logfile = $DOCUMENT_ROOT . "/../configuration/searchlog.txt";
+
+$fp = fopen($logfile, "a");
 if ($fp) {
-   fwrite($fp, date("Y-m-d H:i:s") . " " . $stype . ':' . $query . "\n");
-   fclose($fp);
+	fwrite($fp, date("Y-m-d H:i:s") . " " . $stype . ':' . $query . "\n");
+	fclose($fp);
 } else {
-   print "Please let postmaster@freshports.org know that the search log could not be opened.  This does not affect the search results.\n";
+	print "Please let postmaster@freshports.org know that the search log could not be opened.  This does not affect the search results.\n";
+	define_syslog_variables();
+	syslog(LOG_ERR, "FreshPorts could not open the search log file: $logfile");
 }
 
 
    $query = addslashes($query);
 
-$sql = "select ports.id, ports.name as port, ports.last_update as updated, " .
+$sql = "select ports.id, element.name as port, " .
        "categories.name as category, categories.id as category_id, ports.version as version, ".
-       "ports.last_update_description as update_description, " .
        "ports.maintainer, ports.short_description, ".
-       "ports.package_exists, ports.extract_suffix, ports.needs_refresh, ports.homepage, ports.status, " .
+       "ports.package_exists, ports.extract_suffix, ports.homepage, element.status, " .
        "ports.broken, ports.forbidden " .
-       "from ports, categories  ".
-       "WHERE ports.system = 'FreeBSD' ".
-       "and ports.primary_category_id = categories.id ";
+       "from ports, categories, element  ".
+       "WHERE ports.category_id = categories.id " .
+	   "  and ports.element_id  = element.id " ;
 
 switch ($stype) {
    case "name":
-      $sql .= "and ports.name like '%$query%'";
+      $sql .= "and element.name like '%$query%'";
       break;
 
    case "longdescription":
@@ -80,13 +84,16 @@ switch ($stype) {
       break;
 }
 
-$sql .= " order by ports.name";
+$sql .= " order by categories.name, element.name";
 
-//echo "$sql<br>\n";
+#echo "$sql<br>\n";
 
 
-$result = mysql_query($sql, $db); 
-$NumRows = mysql_num_rows($result);
+$result  = pg_exec($db, $sql);
+$NumRows = pg_numrows($result);
+
+$Port = new Port($db);
+$Port->LocalResult = $result;
 
 }
 ?>
@@ -109,17 +116,21 @@ if ($NumRows == 0) {
 
 //   echo "retrieving $NumRows rows<br>\n";
 
-   for ($i = 0; $i < $NumRows; $i++) {
-      $myrow = mysql_fetch_array($result);
-      $rows[$i]=$myrow;
-//      echo "retrieving row $i<br>\n";
-   }
 
-   for ($i = 0; $i < $NumRows; $i++) {
-//      echo "displaying row $i<br>\n";
-      $myrow = $rows[$i];
+$ShowCategories		= 1;
+GLOBAL	$ShowDepends;
+$ShowDepends		= 1;
+$DaysMarkedAsNew= $DaysMarkedAsNew= $GlobalHideLastChange= $ShowChangesLink= $ShowDescriptionLink= $ShowDownloadPortLink= $ShowHomepageLink= $ShowLastChange= $ShowMaintainedBy= $ShowPortCreationDate= $ShowPackageLink= $ShowShortDescription =1;
+$HideDescription = 0;
+$ShowEverything  = 1;
+$ShowShortDescription = "Y";
+$ShowMaintainedBy     = "Y";
+$GlobalHideLastChange = "Y";
+$ShowDescriptionLink  = "N";
 
-      include("./include/port-basics.php");
+	for ($i = 0; $i < $NumRows; $i++) {
+		$Port->FetchNth($i);
+		$HTML .= freshports_PortDetails($Port, $Port->dbh, $DaysMarkedAsNew, $DaysMarkedAsNew, $GlobalHideLastChange, $HideCategory, $HideDescription, $ShowChangesLink, $ShowDescriptionLink, $ShowDownloadPortLink, $ShowEverything, $ShowHomepageLink, $ShowLastChange, $ShowMaintainedBy, $ShowPortCreationDate, $ShowPackageLink, $ShowShortDescription);
    }
 
 }

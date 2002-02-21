099e18381227b0997814d480e5532f78278f63c6
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2002-02-21 03:17:19 +0000

    we need to include the watch list id on this page. remove some old cache code while here
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@997 46e82423-29d8-e211-989e-002590a4cdd4

--- www/branches/FreshPorts2/watch.php
+++ www/branches/FreshPorts2/watch.php
@@ -1,5 +1,5 @@
 <?
-	# $Id: watch.php,v 1.1.2.6 2002-02-17 23:42:27 dan Exp $
+	# $Id: watch.php,v 1.1.2.7 2002-02-21 03:17:19 dan Exp $
 	#
 	# Copyright (c) 1998-2001 DVL Software Limited
 
@@ -8,6 +8,7 @@
 	require("./include/databaselogin.php");
 	require("./include/getvalues.php");
 
+$Debug = 0;
 
 // if we don't know who they are, we'll make sure they login first
 if (!$visitor) {
@@ -70,44 +71,6 @@ switch ($sort) {
 
 echo "</td></tr>\n";
 
-srand((double)microtime()*1000000);
-$cache_time_rnd =       300 - rand(0, 600);
-
-
-if ($Debug) {
-echo '<br>';
-echo '$cache_file=', $cache_file, '<br>';
-echo '$LastUpdateFile=', $LastUpdateFile , '<br>';
-echo '!(file_exists($cache_file))=',     !(file_exists($cache_file)), '<br>';
-echo '!(file_exists($LastUpdateFile))=', !(file_exists($LastUpdateFile)), "<br>";
-echo 'filectime($cache_file)=',          filectime($cache_file), "<br>";
-echo 'filectime($LastUpdateFile)=',      filectime($LastUpdateFile), "<br>";
-echo '$cache_time_rnd=',                 $cache_time_rnd, '<br>';
-echo 'filectime($cache_file) - filectime($LastUpdateFile) + $cache_time_rnd =', filectime($cache_file) - filectime($LastUpdateFile) + $cache_time_rnd, '<br>';
-}
-
-$UpdateCache = 0;
-if (!file_exists($cache_file)) {
-//   echo 'cache does not exist<br>';
-   // cache does not exist, we create it
-   $UpdateCache = 1;
-} else {
-//   echo 'cache exists<br>';
-   if (!file_exists($LastUpdateFile)) {
-      // no updates, so cache is fine.
-//      echo 'but no update file<br>';
-   } else {
-//      echo 'cache file was ';
-      // is the cache older than the db?
-      if ((filectime($cache_file) + $cache_time_rnd) < filectime($LastUpdateFile)) {
-//         echo 'created before the last database update<br>';
-         $UpdateCache = 1;
-      } else {
-//         echo 'created after the last database update<br>';
-      }
-   }
-}
-
 $UpdateCache = 1;
 
 if ($WatchID == '') {
@@ -128,7 +91,8 @@ $sql = "select ports.id, element.name as port, ports.id as ports_id, commit_log.
        "from watch_list_element, element, categories, ports LEFT OUTER JOIN commit_log on (ports.last_commit_id = commit_log.id) " .
        "WHERE ports.category_id             = categories.id " .
 	   "  and watch_list_element.element_id = ports.element_id " .
-	   "  and ports.element_id              = element.id ";
+	   "  and ports.element_id              = element.id
+          and watch_list_element.watch_list_id = $WatchListID";
 
 $sql .= " order by $sort ";
 //$sql .= " limit 20";
@@ -200,7 +164,7 @@ for ($i = 0; $i < $numrows; $i++) {
 }
 
 if ($ShowCategoryHeaders) {
-	$HTML .= "\n<\DL>\n";
+	$HTML .= "\n</DL>\n";
 }
 
 }

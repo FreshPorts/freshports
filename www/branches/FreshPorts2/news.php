01625dc8c3407fd3b87f45d60e91bfc3e9bdbcda
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2002-03-20 02:03:18 +0000

    convert to use cache table
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@1139 46e82423-29d8-e211-989e-002590a4cdd4

--- www/branches/FreshPorts2/news.php
+++ www/branches/FreshPorts2/news.php
@@ -1,5 +1,5 @@
 <?
-	# $Id: news.php,v 1.1.2.2 2002-02-09 19:42:41 dan Exp $
+	# $Id: news.php,v 1.1.2.3 2002-03-20 02:03:18 dan Exp $
 	#
 	# Copyright (c) 1998-2001 DVL Software Limited
 
@@ -9,157 +9,67 @@
 
 	require("include/getvalues.php");
 
-#$Debug=1;
-#echo 'stuff';
-#exit;
+#	$Debug=1;
+#	echo 'stuff';
+#	exit;
 
-#phpinfo();
+#	phpinfo();
 
-$text_file	=	"./news.txt";
+	$ServerName = str_replace("freshports", "FreshPorts", $SERVER_NAME);
 
-#require("./include/commonlogin.php3");
-#$Debug=1;
+	$MyMaxArticles = 10;
 
-$ServerName = str_replace("freshports", "FreshPorts", $SERVER_NAME);
+	if (!$MaxArticles || $MaxArticles < 1 || $MaxArticles > $MyMaxArticles) {
+		$MaxArticles = $MyMaxArticles;
+	}
 
-$MyMaxArticles = 10;
+	if ($MaxArticles == $MyMaxArticles) {
+		$OutputFromCach = 1;
+	} else {
+		$OutputFromCach = 0;
+	}
 
-if (!$MaxArticles || $MaxArticles < 1 || $MaxArticles > $MyMaxArticles) {
-    $MaxArticles = $MyMaxArticles;
-}
+	$HTML .= '<!DOCTYPE rss PUBLIC "-//Netscape Communications//DTD RSS 0.91//EN"' . "\n";
+	$HTML .= '        "http://my.netscape.com/publish/formats/rss-0.91.dtd">' . "\n";
+	$HTML .= '<rss version="0.91">' . "\n";
 
-if ($MaxArticles == $MyMaxArticles) {
-   $OutputFromCach = 1;
-} else {
-   $OutputFromCach = 0;
-}
+	$HTML .= "\n";
 
-#if ($BuildTable == 1) {
-   $HTML .= '<!DOCTYPE rss PUBLIC "-//Netscape Communications//DTD RSS 0.91//EN"' . "\n";
-   $HTML .= '        "http://my.netscape.com/publish/formats/rss-0.91.dtd">' . "\n";
-   $HTML .= '<rss version="0.91">' . "\n";
+	$HTML .= '<channel>' . "\n";
+	$HTML .= '  <title>FreshPorts - the place for ports</title>' . "\n";
+	$HTML .= '  <link>http://' . $ServerName . '/</link>' . "\n";
+	$HTML .= '  <description>The easiest place to find ports</description>' . "\n";
+	$HTML .= '  <language>en-us</language>' . "\n";
 
-   $HTML .= "\n";
+	$sort ="commit_log.commit_date desc, commit_log.id asc, element.name, category, version";
 
-   $HTML .= '<channel>' . "\n";
-   $HTML .= '  <title>FreshPorts - the place for ports</title>' . "\n";
-   $HTML .= '  <link>http://' . $ServerName . '/</link>' . "\n";
-   $HTML .= '  <description>The easiest place to find ports</description>' . "\n";
-   $HTML .= '  <language>en-us</language>' . "\n";
+	$sql = "select * from commits_latest order by commit_date_raw desc, category, port";
 
-$sort ="commit_log.commit_date desc, commit_log.id asc, element.name, category, version";
+	$sql .= " limit 20";
 
-$sql = "
-select DISTINCT commit_log.commit_date as commit_date_raw,
-       commit_log.id as commit_log_id,
-       commit_log.description as commit_description,
-       to_char(commit_log.commit_date + INTERVAL '$CVSTimeAdjustment seconds', 'DD Mon YYYY') as commit_date,
-       to_char(commit_log.commit_date + INTERVAL '$CVSTimeAdjustment seconds', 'HH24:MI') as commit_time,
-	   commit_log_ports.port_id as port_id,
-	   categories.name as category,
-	   categories.id   as category_id,
-	   element.name    as port,
-	   commit_log_ports.port_version   as version,
-	   element.status    as status,
-	   commit_log_ports.needs_refresh  as needs_refresh,
-	   ports.forbidden      as forbidden,
-	   ports.broken         as broken,
-	   date_part('epoch', ports.date_added) as date_added
-  from commit_log_ports, commit_log, ports, element, categories
- where commit_log.commit_date         > '" . date("Y-m-d", time() - 60*60*24*14) . "'
-   and commit_log_ports.commit_log_id = commit_log.id
-   and commit_log_ports.port_id       = ports.id
-   and categories.id                  = ports.category_id
-   and element.id                     = ports.element_id";
+	if ($Debug) {
+		echo $sql;
+		}
 
+	$result = pg_exec ($db, $sql);
+	if (!$result) {
+		echo $sql . 'error = ' . pg_errormessage();
+		exit;
+	}
 
-#$AnnounceFile = "news.announcement.txt";
-#if (file_exists($AnnounceFile) && filesize($AnnounceFile) > 4) {
-   $sql .= " order by $sort limit 19";
-   $HTML .= '  <item>' . "\n";
-   $HTML .= '    <title>FreshPorts HEADS UP!</title>' . "\n";
-   $HTML .= '    <link>http://' . $ServerName . '/</link>' . "\n";
-   $HTML .= '    <description>the place for ports - same great content; far faster delivery</description>' . "\n";
-   $HTML .= '  </item>' . "\n";
+	$numrows = pg_numrows($result);
+	for ($i = 0; $i < $numrows; $i++) {
+		$myrow = pg_fetch_array ($result, $i);
+		$HTML .= '  <item>' . "\n";
+		$HTML .= '    <title>' . $myrow["category"] . '/' . $myrow["port"] . '</title>' . "\n";
+		$HTML .= '    <link>http://' . $ServerName . '/' . $myrow["category"] . '/' . $myrow["port"] . '/</link>' . "\n";
+		$HTML .= '    <description>' . htmlspecialchars(trim($myrow["commit_description"])) . '</description>' . "\n";
+		$HTML .= '  </item>' . "\n";
+	}
 
-   $TEXT .= 'Please take our survey' . "\n";
-   $TEXT .= 'http://freshports.org/survey/' . "\n";
-#} else { */
-#   $sql .= " order by $sort limit 20";
-#}
-
-if ($Debug) {
-   echo $sql;
-}
-
-   $result = pg_exec ($db, $sql);
-   if (!$result) {
-      echo $sql . 'error = ' . pg_errormessage();
-      exit;
-   }
-
-   $i=0;
-   $numrows = pg_numrows($result);
-   for ($i = 0; $i < $numrows; $i++) {
-      $myrow = pg_fetch_array ($result, $i);
-      $HTML .= '  <item>' . "\n";
-      $HTML .= '    <title>' . $myrow["category"] . '/' . $myrow["port"] . '</title>' . "\n";
-      $HTML .= '    <link>http://' . $ServerName . '/' . $myrow["category"] . '/' . $myrow["port"] . '/</link>' . "\n";
-      $HTML .= '    <description>' . htmlspecialchars(trim($myrow["update_description"])) . '</description>' . "\n";
-      $HTML .= '  </item>' . "\n";
-
-      $TEXT .= $myrow["category"] . '/' . $myrow["port"] . "\n";
-      $TEXT .= 'http://' . $ServerName . '/' . $myrow["category"] . '/' . $myrow["port"] . "/\n";
-   }
-
-   $HTML .= '</channel>' . "\n";
-   $HTML .= '</rss>' . "\n";
+	$HTML .= '</channel>' . "\n";
+	$HTML .= '</rss>' . "\n";
    
-#}
-
-/*
-if ($UpdateCache == 1) {
-   // output $HTML the cache
-   $fpwrite = fopen($cache_file, 'w');
-   if(!$fpwrite) {
-     echo "$errstr ($errno)<br>\n";
-     exit;
-   } else {
-       fputs($fpwrite, $HTML);
-       fclose($fpwrite);
-   }
-
-   // output the text file now too
-   $fpwrite = fopen($text_file, 'w');
-   if(!$fpwrite) {
-     echo "$errstr ($errno)<br>\n";
-     exit;
-   } else {
-       fputs($fpwrite, $TEXT);
-       fclose($fpwrite);
-   }
-
-}
-
-if ($OutputFromCach == 1) {
-   
-   if ($Debug) {
-      echo "outputting from cache ==> ", $cache_file, "<br>\n";
-   }
-   // give them the cach
-   if (file_exists($cache_file)) {
-      echo '<?xml version="1.0"?>', "\n";
-      include($cache_file);
-   }
-} else {
-   if ($Debug) {
-      echo "giving them the HTML", "<br>\n";
-   }
-   // give them the custom output
-*/
-   echo '<?xml version="1.0"?>', "\n";
-   echo $HTML;
-#}
-
-
+	echo '<?xml version="1.0"?>', "\n";
+	echo $HTML;
 ?>

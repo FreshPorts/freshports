cd2e4297b8aebe6f20265338b927bda08f193ac6
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2002-03-30 08:45:40 +0000

    doh, change the right line
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@1176 46e82423-29d8-e211-989e-002590a4cdd4

--- include/branches/FreshPorts2/freshports.php
+++ include/branches/FreshPorts2/freshports.php
@@ -1,6 +1,6 @@
 <?
 
-   # $Id: freshports.php,v 1.4.2.52 2002-03-30 08:43:42 dan Exp $
+   # $Id: freshports.php,v 1.4.2.53 2002-03-30 08:45:40 dan Exp $
    #
    # Copyright (c) 1998-2002 DVL Software Limited
 
@@ -817,9 +817,8 @@ function freshports_PortCommitsHeader($port) {
 	# print the header for the commits for a port
 
 	echo '<TABLE BORDER="1" width="100%" CELLSPACING="0" CELLPADDING="5"bordercolor="#a2a2a2" bordercolordark="#a2a2a2" bordercolorlight="#a2a2a2">' . "\n";
-#	echo '<tr height="20"><td colspan="3" bgcolor="#AD0040"><font color="#FFFFFF"><font size="+1">Commit History</font> (may be incomplete: see CVSWeb link above for full details)</font></td></tr>' . "\n";
 
-	freshports_PageBannerText("Commit History - (may be incomplete: see Changes link above for full details)", 3);
+	freshports_PageBannerText("Commit History - (may be incomplete: see CVSWeb link above for full details)", 3);
 
 	echo "<TR><TD WIDTH=\"180\"><b>Date</b></td><td><b>Committer</b></td><td><b>Description</b></td></tr>\n";
 }

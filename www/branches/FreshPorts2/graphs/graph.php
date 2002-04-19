6f4482692f66b1a69cc0f1b84a6417500e1a8411
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2002-04-19 18:54:35 +0000

    First commit
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@1300 46e82423-29d8-e211-989e-002590a4cdd4

---
+++ www/branches/FreshPorts2/graphs/graph.php
@@ -0,0 +1,105 @@
+<?
+	# $Id: graph.php,v 1.1.2.1 2002-04-19 18:54:34 dan Exp $
+	#
+
+// parameters: graph id
+require("bar-graphs.php");
+
+// --------------------------------------------------------------------
+// FreshPorts bar chart
+
+// $values must be an array of numbers
+function FreshPortsChart($title, $axislabel, $values, $labels, $urls, $file = "-")
+{
+        $c = new dg_BarGraph();
+        $c->width = 500;
+        $c->values = $values;
+        $c->labels = $labels;
+		$c->urls = $urls;
+		$c->title = $title;
+		$c->axis_label = $axislabel;
+		$c->gradient1 = array(180,0,0); // from dark red
+        $c->gradient2 = array(255,255,0); // to bright yellow
+
+        $c->footer = "(c) http://www.FreshPorts.org/                               " . date("Y-m-d G:i:s");
+        return $c->show($file);
+}
+
+
+// parameters:
+// id=number of graph
+
+// assume that we always have graph of id=0
+if (!isset($id)) $id=0;
+
+$fid = "graph$id";
+$cache_dir = "/tmp/";
+$period = 5; // in seconds
+
+$filename = $cache_dir.$fid.".png";
+if (!file_exists($filename) || filemtime($filename)+$period<time())	
+{
+	// get graph information
+	$conn = @pg_connect("dbname=fp2migration")
+		or die("PGERR: cannot connect");
+
+	// XXX CHANGE THE QUERY XXX
+	$data = @pg_exec($conn,"select query, title, title from graphs where id=$id")
+		or die("PGERR 1: ".pg_ErrorMessage());
+	
+	if (pg_numrows($data) == 0)
+		die("GRAPH: invalid id");
+
+	$r = pg_fetch_row($data, $i);
+
+	$query = $r[0];
+	$title = $r[1];
+	$axislabel = $r[2];
+
+	pg_freeresult($data);
+
+	// get graph data
+	$data = @pg_exec($conn,$query)
+		or die("PGERR 2: ".pg_ErrorMessage());
+
+	$v = array();
+	$l = array();
+	$u = array();
+
+	for ($i=0; $i<pg_numrows($data); $i++)
+	{
+       	$r = pg_fetch_row($data, $i);
+	    array_push($v,$r[1]);
+		array_push($l,$r[0]."  ");
+		array_push($u,$r[2]); // XXX CHANGE IT TO 3 XXX
+	}
+	
+	pg_freeresult($data);
+	
+	// draw
+	$map = FreshPortsChart($title, $axislabel, $v, $l, $u, $filename);
+
+	// save map
+	$fp = fopen($cache_dir.$fid.".map","w");
+	fputs($fp,$map);
+	fclose($fp);
+	
+	pg_close();
+}
+
+
+header("Content-type: image/png");
+readfile($filename);
+
+
+//  CREATE TABLE "graph" ("id" integer NOT NULL, "query" text NOT NULL,
+//  "title" text NOT NULL);
+
+// insert into graph (id, query, title) values (0,'select category,
+// name, count(watch_list_element.element_id) from ports_active,
+// watch_list_element where ports_active.element_id =
+// watch_list_element.element_id group by category, name order by 3 desc
+// limit 20','Most Watched Ports');
+
+
+?>

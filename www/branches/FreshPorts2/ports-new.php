e74190915f646315dfa720d6a2926b9914dddd31
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2002-04-22 03:32:50 +0000

    All new, based on ports-deleted.php
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@1345 46e82423-29d8-e211-989e-002590a4cdd4

--- www/branches/FreshPorts2/ports-new.php
+++ www/branches/FreshPorts2/ports-new.php
@@ -1,5 +1,5 @@
 <?
-	# $Id: ports-new.php,v 1.1.2.4 2002-02-16 23:52:51 dan Exp $
+	# $Id: ports-new.php,v 1.1.2.5 2002-04-22 03:32:50 dan Exp $
 	#
 	# Copyright (c) 1998-2001 DVL Software Limited
 
@@ -8,142 +8,112 @@
 	require("./include/databaselogin.php");
 	require("./include/getvalues.php");
 
-	freshports_Start("recently added ports",
+	$Debug = 0;
+
+	$Interval = '3 months';
+	$Title    = "New ports - past " . $Interval;
+
+	freshports_Start($Title,
 					"freshports - new ports, applications",
 					"FreeBSD, index, applications, ports");
 
-$Debug=1;
 ?>
 
-<table width="<? echo $TableWidth ?>" border="0" ALIGN="center">
-<tr><td valign="top" width="100%">
-<table width="100%" border="0">
-<tr>
-	<? freshports_PageBannerText("$FreshPortsTitle - recently added ports", 4); ?>
-  </tr>
-<tr><td colspan="2">
-This page shows the ports which have been recently added to the ports tree.  As such, some information
-may be missing (such as description, maintainer, etc.). This information will be obtained during the
-next database update.
-</td></tr>
-<tr><td>
+<TABLE width="<? echo $TableWidth ?>" border="0" ALIGN="center">
+
+<TR><TD valign="top" width="100%">
+<TABLE width="100%" border="0">
+<TR>
+	<? freshports_PageBannerText($Title); ?>
+</TR>
+<TR><TD>
+These are the recently added ports.
+</TD></TR>
 <?
 
-$DESC_URL = "ftp://ftp.freebsd.org/pub/FreeBSD/branches/-current/ports";
+	$DESC_URL = "ftp://ftp.freebsd.org/pub/FreeBSD/branches/-current/ports";
 
-// make sure the value for $sort is valid
+	if ($visitor) {
+		$WatchID = freshports_MainWatchID($UserID, $db);
+	} else {
+		unset ($WatchID);
+	}
 
-switch ($sort) {
-/* sorting by port is disabled. Doesn't make sense to do this
-   case "port":
-      $sort = "port, updated desc";
-      $cache_file .= ".port";
-      break;
-*/
-//   case "updated":
-//      $sort = "updated desc, port";
-//      break;
+		// make sure the value for $sort is valid
 
-   default:
-      $sort ="date_added desc, port";
-      $cache_file .= ".updated";
-}
+		echo "<TR><TD>\nThis page is ";
 
-srand((double)microtime()*1000000);
-$cache_time_rnd =       300 - rand(0, 600);
+		switch ($sort) {
+			case "dateadded":
+				$sort = "ports.date_added desc, category, port";
+				echo 'sorted by date added.  but you can sort by <a href="' . $PHP_SELF . '?sort=category">category</a>';
+				$ShowCategoryHeaders = 0;
+				break;
 
+			default:
+				$sort ="category, port";
+				echo 'sorted by category.  but you can sort by <a href="' . $PHP_SELF . '?sort=dateadded">date added</a>';
+				$ShowCategoryHeaders = 1;
+		}
 
-$UpdateCache = 0;
-if (!file_exists($cache_file)) {
-//   echo 'cache does not exist<br>';
-   // cache does not exist, we create it
-   $UpdateCache = 1;
-} else {
-//   echo 'cache exists<br>';
-   if (!file_exists($LastUpdateFile)) {
-      // no updates, so cache is fine.
-//      echo 'but no update file<br>';
-   } else {
-//      echo 'cache file was ';
-      // is the cache older than the db?
-      if ((filectime($cache_file) + $cache_time_rnd) < filectime($LastUpdateFile)) {
-//         echo 'created before the last database update<br>';
-         $UpdateCache = 1;
-      } else {
-//         echo 'crated after the last database update<br>';
-      }
-   }
-}
+		echo "</TD></TR>\n";
 
-$UpdateCache = 1;
+			$sql = "select ports.id, element.name as port,  " .
+			       "categories.name as category, ports.category_id, version as version, revision as revision, ".
+			       " ports.element_id, " .
+			       "maintainer, short_description, 
+					to_char(ports.date_added - SystemTimeAdjust(), 'DD Mon YYYY HH24:MI:SS') as date_added, ".
+			       "last_commit_id as last_change_log_id, " .
+			       "package_exists, extract_suffix, homepage, status, " .
+			       "broken, forbidden ";
 
-if ($UpdateCache == 1) {
-//   echo 'time to update the cache';
+			if ($WatchListID) {
+				$sql .= ", CASE when watch_list_element.element_id is null
+								then 0
+								else 1
+							END as watch ";
+			}
 
-$sql = "select ports.id, ports.name as port, " .
-       "categories.name as category, categories.id as category_id, ports.version as version, ".
-       "ports.last_update_description as update_description, " .
-       "ports.maintainer, ports.short_description, UNIX_TIMESTAMP(ports.date_added) as date_added, ".
-       "date_format(date_added, '$FormatDate $FormatTime') as date_added_formatted, ".
-       "ports.package_exists, ports.extract_suffix, ports.needs_refresh, ports.homepage, ports.status, " .
-       "ports.broken, ports.forbidden " .
-       "from ports, categories ".
-       "WHERE ports.system = 'FreeBSD' ".
-       "  and ports.primary_category_id       = categories.id " .
-       "  and ports.status                    = 'A' ";
+			$sql .= " from element, categories, ports   ";
 
-/*
-$sql .= "order by $sort limit $MaxNumberOfPorts";
+			if ($WatchListID) {
+					$sql .= " left outer join watch_list_element
+							 ON (ports.element_id        = watch_list_element.element_id
+							AND watch_list_element.watch_list_id = $WatchListID) ";
+			}
+			$sql .= "WHERE ports.element_id = element.id
+					   and ports.category_id = categories.id 
+                       and status = 'A' 
+					   and ports.date_added > (now() - interval '$Interval') ";
 
-if ($Debug) {
-   echo $sql;
-}
+			$sql .= " order by $sort ";
+#			$sql .= " limit 20";
 
-$result = mysql_query($sql, $db);
+			if ($Debug) {
+				echo $sql;
+			}
 
-// get the list of topics, which we need to modify the order
-$NumTopics=0;
-$ShowLastChange="N";
-while ($myrow = mysql_fetch_array($result)) {
-   include("./include/port-basics.php");
-}
+			$result = pg_exec($db, $sql);
+			if (!$result) {
+				echo pg_errormessage();
+			} else {
+				$numrows = pg_numrows($result);
+#				echo "There are $numrows to fetch<BR>\n";
+			}
 
-//$HTML .= '</tr>';
+			require("./include/list-of-ports.php");
 
-//$HTML .= "</table>\n";
+			echo freshports_ListOfPorts($result, $db, "Y", $ShowCategoryHeaders);
+?>
 
-mysql_free_result($result);
+</TABLE>
 
-
-echo $HTML;
-
-   $fpwrite = fopen($cache_file, 'w');
-   if(!$fpwrite) {
-      echo 'error on open<br>';
-      echo "$errstr ($errno)<br>\n";
-      exit;
-   } else {
-//      echo 'written<br>';
-      fputs($fpwrite, $HTML);
-      fclose($fpwrite);
-   }
-} else {
-//   echo 'looks like I\'ll read from cache this time';
-   if (file_exists($cache_file)) {
-      include($cache_file);
-   }
-*/
-echo '<tr><td>Sorry, but we\'ve disabled this page. Sorry about that. With luck, it will be back in FreshPorts2.</td></tr>';
-}
-
-</script>
-</table>
-</td>
-  <td valign="top" width="*">
-   <? include("./include/side-bars.php") ?>
- </td>
-</tr>
-</table>
+</TD>
+  <TD valign="top" width="*">
+<? include("./include/side-bars.php") ?>
+</TD>
+</TR>
+</TABLE>
 
 <TABLE WIDTH="<? echo $TableWidth; ?>" BORDER="0" ALIGN="center">
 <TR><TD>
@@ -153,3 +123,4 @@ echo '<tr><td>Sorry, but we\'ve disabled this page. Sorry about that. With luck,
 
 </body>
 </html>
+

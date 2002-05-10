77f19ed579a91449857363370c5a0be325132f31
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2002-05-10 23:32:30 +0000

    general tidy of HTML code
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@1386 46e82423-29d8-e211-989e-002590a4cdd4

--- www/branches/FreshPorts2/about.php
+++ www/branches/FreshPorts2/about.php
@@ -1,5 +1,5 @@
 <?
-	# $Id: about.php,v 1.1.2.5 2002-04-19 19:46:43 dan Exp $
+	# $Id: about.php,v 1.1.2.6 2002-05-10 23:32:30 dan Exp $
 	#
 	# Copyright (c) 1998-2001 DVL Software Limited
 
@@ -14,7 +14,7 @@
 
 ?>
 <TABLE WIDTH="<? echo $TableWidth; ?>" BORDER="0" ALIGN="center">
-<tr><td VALIGN=TOP>
+<TR><td VALIGN=TOP>
 <TABLE>
 <TR>
 	<? freshports_PageBannerText("About this site"); ?>
@@ -26,65 +26,78 @@ We have here a few notes about this website.
 </P>
 </TD></TR>
 
-<tr>
+<TR>
 <td valign="top" width="100%">
 
-<tr>
-	<? freshports_PageBannerText("What is a port?"); ?>
-</tr>
+<TR>
+	<? 
+	freshports_BannerSpace();
+	freshports_PageBannerText("What is a port?"); 
+	?>
+</TR>
 
-<tr><td>
+<TR><TD>
 
-<p>A port is the term used to describe a collection of files which makes it extremely
-easy to install an application.  As it says in the <a href="http://www.freebsd.org/ports/">
-FreeBSD Ports description</a>: <em>Installing an application is as simple as downloading 
+<P>A port is the term used to describe a collection of files which makes it extremely
+easy to install an application.  As it says in the <A HREF="http://www.freebsd.org/ports/">
+FreeBSD Ports description</A>: <em>Installing an application is as simple as downloading 
 the port, unpacking it and typing <b>make</b> in the port directory</em>. If you want an application, 
-the port is the Way To Go(TM)</p>
+the port is the Way To Go(TM)</P>
 
-<p>So off you go to the ports tree to install your favourite port.  It's quite easy. It's simple.
+<P>So off you go to the ports tree to install your favourite port.  It's quite easy. It's simple.
 And you love that new application.  And you want to know when the port is updated.  That's where
-we come in.</p>
+we come in.</P>
 
-<p>For more information about the Ports tree, see <a href="http://www.freebsd.org/ports/">http://www.freebsd.org/ports/</a>.</p>
+<P>For more information about the Ports tree, see <A HREF="http://www.freebsd.org/ports/">http://www.freebsd.org/ports/</A>.</P>
 
-</td></tr>
-<tr>
-	<? freshports_PageBannerText("What is $FreshPortsTitle"); ?>
-</tr>
+</TD></TR>
+<TR>
+	<?
+	freshports_BannerSpace();
+	freshports_PageBannerText("What is $FreshPortsTitle");
+	?>
+</TR>
 
-<tr><td>
+<TR><TD>
 
-<p><? echo $FreshPortsTitle; ?> lists the change made to the ports tree. If you wish, <? echo $FreshPortsTitle; ?> can email you 
+<P><? echo $FreshPortsTitle; ?> lists the change made to the ports tree. If you wish, <? echo $FreshPortsTitle; ?> can email you 
 when your favourite port has been updated.
-</p>
+</P>
 
 <P>
 <? echo $FreshPortsTitle; ?> is not the place to report errors or request changes.  You should do that on the 
-<A HREF="mailto:freebsd-ports&#64;freebsd.org">FreeBSD Ports mailing list</A>.  We do not maintain ports.  We do not
+<A HREF="<? GLOBAL $mailto; echo $mailto; ?>:freebsd-ports&#64;freebsd.org">FreeBSD Ports mailing list</A>.  We do not maintain ports.  We do not
 create ports.  We do not fix ports.  We just tell you what others have been doing to the Ports tree.
 </P>
 
-<td></tr>
-<tr>
-	<? freshports_PageBannerText("OK, whose bright idea was this?"); ?>
-</tr>
+</TD></TR>
 
-<tr><td>
-<p>This site was created by Dan Langille.  His other web feats include 
-<a href="http://www.freebsddiary.org/">The FreeBSD Diary</a>, <a 
-href="http://www.racingsystem.com">The Racing System</a>, and an ability
-to avoid reading the inane comments on <a href="http://slashdot.org">slashdot</a>.
-But Dan didn't create the site all by himself.  Have a look at <a href="authors.php">
-About the Authors</a> for details of who else helped.</p>
-</table>
-</td>
-  <td valign="top">
+
+<TR>
+	<?
+	freshports_BannerSpace();
+	freshports_PageBannerText("OK, whose bright idea was this?");
+	?>
+</TR>
+
+<TR><TD>
+<P>This site was created by Dan Langille.  His other web feats include 
+<A HREF="http://www.freebsddiary.org/">The FreeBSD Diary</A>, <a 
+href="http://www.racingsystem.com">The Racing System</A>, and an ability
+to avoid reading the inane comments on <A HREF="http://slashdot.org">slashdot</A>.
+But Dan didn't create the site all by himself.  Have a look at <A HREF="authors.php">
+About the Authors</A> for details of who else helped.</P>
+</TD></TR>
+
+</TABLE>
+</TD>
+  <TD ALIGN="top">
     <?
        $ShowPoweredBy = 1;
        include("./include/side-bars.php");
     ?>
- </td>
-</table>
+ </TD>
+</TABLE>
 
 <TABLE WIDTH="<? echo $TableWidth; ?>" BORDER="0" ALIGN="center">
 <TR><TD>
@@ -92,5 +105,5 @@ About the Authors</a> for details of who else helped.</p>
 </TD></TR>
 </TABLE>
 
-</body>
-</html>
+</BODY>
+</HTML>

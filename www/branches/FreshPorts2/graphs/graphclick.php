b228ad490e359212a55a1d9ebe81c1a8f8259012
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2002-05-19 21:59:03 +0000

    start using $_SERVER
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@1447 46e82423-29d8-e211-989e-002590a4cdd4

--- www/branches/FreshPorts2/graphs/graphclick.php
+++ www/branches/FreshPorts2/graphs/graphclick.php
@@ -1,40 +1,43 @@
 <?
-	# $Id: graphclick.php,v 1.1.2.2 2002-04-20 04:55:50 dan Exp $
+	# $Id: graphclick.php,v 1.1.2.3 2002-05-19 21:59:03 dan Exp $
 	#
 
-$cache_dir = "/tmp/";
+	$cache_dir = "/tmp/";
 
-if (!isset($id)) $id=0;
 
-$map = file($cache_dir."FreshPorts.graph".$id.".map");
-if (count($map) == 0) {
-	die("GRAPH: invalid id");
-}
+	$id      = $_GET["id"];
+	$graph_x = $_GET["graph_x"];
+	$graph_y = $_GET["graph_y"];
 
-foreach ($map as $m) {
-	list($y,$p) = split(":",$m);
-	$map_y[] = $y;
-	$map_p[] = $p;
-}
+	if (!isset($id)) $id=0;
 
-$i = 0;
-while ($i < count($map) && $graph_y>$map_y[$i]) {
-	$i++;
-}
-
-// click out of bars (too high or too low)
-if ($i==0 || $i==count($map)) {
-	if (!isset($HTTP_REFERER) || $HTTP_REFERER=="") {
-		header("Location: http://".$_SERVER['HTTP_HOST']);
-	} else {
-		header("Location: $HTTP_REFERER");
+	$map = file($cache_dir."FreshPorts.graph".$id.".map");
+	if (count($map) == 0) {
+		die("GRAPH: invalid id");
 	}
-	exit;
-}
 
-$URL = $map_p[$i-1];
+	foreach ($map as $m) {
+		list($y,$p) = split(":",$m);
+		$map_y[] = $y;
+		$map_p[] = $p;
+	}
 
-header("Location: http://"
-	.$_SERVER['HTTP_HOST']
-	."$URL");
+	$i = 0;
+	while ($i < count($map) && $graph_y>$map_y[$i]) {
+		$i++;
+	}
+
+	// click out of bars (too high or too low)
+	if ($i==0 || $i==count($map)) {
+		if (!isset($_SERVER["HTTP_REFERER"]) || $_SERVER["HTTP_REFERER"] == '') {
+			header("Location: http://" . $_SERVER['HTTP_HOST']);
+		} else {
+			header("Location: . $_SERVER["HTTP_REFERER"]);
+		}
+		exit;
+	}
+
+	$URL = $map_p[$i-1];
+
+	header("Location: http://" . $_SERVER['HTTP_HOST'] . "$URL");
 ?>

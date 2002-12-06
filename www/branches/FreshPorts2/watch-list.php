012f305135c35fd12856f492a02b29077a5c154f
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2002-12-06 21:23:09 +0000

    we now need a watch list id when adding removing items improve security by linking the SQL to the USER ID
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@1784 46e82423-29d8-e211-989e-002590a4cdd4

--- www/branches/FreshPorts2/watch-list.php
+++ www/branches/FreshPorts2/watch-list.php
@@ -1,5 +1,5 @@
 <?
-	# $Id: watch-list.php,v 1.2.2.7 2002-05-22 04:30:29 dan Exp $
+	# $Id: watch-list.php,v 1.2.2.8 2002-12-06 21:23:09 dan Exp $
 	#
 	# Copyright (c) 1998-2002 DVL Software Limited
 
@@ -21,13 +21,25 @@
 		header("Location: $Origin");  /* Redirect browser to PHP web site */
 		exit;  /* Make sure that code below does not get executed when we redirect. */
 	}
+	
+	if (IsSet($_GET["wlid"])) {
+		$WatchListID= AddSlashes($_GET["wlid"]);
+	} else {
+		die("Watch List ID not supplied when modifying watch list");
+	}
 
 	if (IsSet($_GET["remove"])) {
-		$remove = AddSlashes($_GET["remove"]);
-		if ($Debug) echo "I'm removing $remove\n<BR>";
-		$sql = "delete from watch_list_element
-			     where watch_list_id = $WatchListID
-				   and element_id    = $remove";
+		$ElementID= AddSlashes($_GET["remove"]);
+		if ($Debug) echo "I'm removing $ElementID\n<BR>";
+		#
+		# The subselect ensures the user can only delete things from their
+		# own watch list
+		#
+		$sql = "DELETE FROM watch_list_element
+		         WHERE watch_list_element.element_id     = $ElementID
+		            AND watch_list.id                    = $WatchListID
+		            AND watch_list.user_id               = $UserID
+		            AND watch_list_element.watch_list_id = watch_list.id";
 
 		$result = pg_exec($db, $sql);
 		if (!$result) {
@@ -37,13 +49,18 @@
 	}
 
 	if (IsSet($_GET["add"])) {
-		$add = AddSlashes($_GET["add"]);
+		$ElementID = AddSlashes($_GET["add"]);
 		if ($Debug) echo "I'm adding $add\n<BR>";
-		$sql = "insert into watch_list_element (watch_list_id, element_id) values
-								($WatchListID, $add)";
+		$sql = "insert into watch_list_element (watch_list_id, element_id)
+		        values (
+		             (SELECT id
+		               FROM watch_list
+		              WHERE user_id = $UserID
+		                AND id      = $WatchListID), $ElementID)";
+        
 		$result = pg_exec($db, $sql);
 		if (!$result) {
-			echo pg_errormessage();
+			echo pg_errormessage() . $sql;
 			$Redirect = 0;
 		}
 		

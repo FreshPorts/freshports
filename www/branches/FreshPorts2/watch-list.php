ae2ca6129fe225e11e412bc7d10f34d6576f3ef9
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2002-12-08 16:47:59 +0000

    Correctly go back to the referrer when Asking. Catch and ignore duplicate entry errors. Display the port details.
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@1806 46e82423-29d8-e211-989e-002590a4cdd4

--- www/branches/FreshPorts2/watch-list.php
+++ www/branches/FreshPorts2/watch-list.php
@@ -1,5 +1,5 @@
 <?
-	# $Id: watch-list.php,v 1.2.2.9 2002-12-08 03:22:45 dan Exp $
+	# $Id: watch-list.php,v 1.2.2.10 2002-12-08 16:47:59 dan Exp $
 	#
 	# Copyright (c) 1998-2002 DVL Software Limited
 
@@ -9,10 +9,13 @@
 
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/include/getvalues.php");
 
-	$Debug = 1;
-	$origin = $_GET["origin"];
+	$Debug = 0;
 
-	$Origin   = $HTTP_SERVER_VARS["HTTP_REFERER"];
+	if ($_POST["Origin"]) {
+		$Origin = AddSlashes($_POST["Origin"]);
+	} else {
+		$Origin = $HTTP_SERVER_VARS["HTTP_REFERER"];
+	}
 	$Redirect = 1;
 #phpinfo();
 
@@ -31,7 +34,7 @@ function RemoveElementFromWatchLists($db, $UserID, $ElementID, $WatchListsIDs) {
 
 		if ($Debug) echo "<pre>$sql</pre>";
 		$result = pg_exec($db, $sql);
-		if (!result) {
+		if (!$result) {
 			break;
 		}
 	}
@@ -39,12 +42,17 @@ function RemoveElementFromWatchLists($db, $UserID, $ElementID, $WatchListsIDs) {
 	return $result;
 }
 
-function AddElementToWatchLists($db, $UserId, $ElementID, $WatchListsIDs) {
+function AddElementToWatchLists($db, $UserID, $ElementID, $WatchListsIDs) {
 	#
 	# The subselect ensures the user can only delete things from their
 	# own watch list
 	#
-$Debug = 1;
+	$Debug = 0;
+
+	#
+	# make sure we don't report the duplicate entry error when adding...
+	#
+	$PreviousReportingLevel = error_reporting(E_ALL ^ E_WARNING);
 	if ($Debug) echo "I'm adding $ElementID\n<BR>";
 	while (list($key, $WatchListID) = each($WatchListsIDs)) {
 		$sql = "insert into watch_list_element (watch_list_id, element_id)
@@ -56,10 +64,16 @@ $Debug = 1;
         
 		if ($Debug) echo "<pre>$sql</pre>";
 		$result = pg_exec($db, $sql);
-		if (!result) {
-			break;
+		if (!$result) {
+			# If this isn't s aduplicate key error, then break
+			if (stristr(pg_last_error(), "Cannot insert a duplicate key") == '') {
+				break;
+			} else {
+				$result = 1;
+			}
 		}
 	}
+	error_reporting($PreviousReportingLevel);
 
 	return $result;
 }
@@ -70,23 +84,19 @@ $Debug = 1;
 		header("Location: $Origin");  /* Redirect browser to PHP web site */
 		exit;  /* Make sure that code below does not get executed when we redirect. */
 	}
-	if (IsSet($_GET["wlid"])) {
-		$WatchListID = AddSlashes($_GET["wlid"]);
-	} else {
-phpinfo();
-		die("Watch List ID not supplied when modifying watch list");
-	}
 
 	if (IsSet($_GET["ask"])) {
 		require_once($_SERVER['DOCUMENT_ROOT'] . "/include/watch-lists.php");
+		require_once($_SERVER['DOCUMENT_ROOT'] . "/../classes/ports.php");
 
 		freshports_Start("Watch list maintenance",
 						"freshports - new ports, applications",
 						"FreeBSD, index, applications, ports");
 		?>
+
 <TABLE WIDTH="<? echo $TableWidth; ?>" BORDER="0" ALIGN="center">
 <TR><td VALIGN=TOP>
-<TABLE border="0">
+<TABLE border="0" width="100%">
 <TR>
 	<? freshports_PageBannerText("Watch list maintenance"); ?>
 </TR>
@@ -100,23 +110,42 @@ phpinfo();
 	if (IsSet($_GET["remove"])) {
 		$ButtonName = "Remove";
 		$Action     = "remove";
+		$Verb       = 'removed';
+		$FromTo     = 'from';
 		$Object     = AddSlashes($_GET["remove"]);
 	} else {
 		if (IsSet($_GET["add"])) {
 			$ButtonName = "Add";
 			$Action     = "add";
+			$Verb       = 'added';
+			$FromTo      = 'to';
 			$Object     = AddSlashes($_GET["add"]);
 		} else {
 			die("I don't know whether you are removing or adding, so I'll just stop here shall I?");
 		}
 	}
-
+	$ShowCategories			= 1;
+	GLOBAL $ShowDepends;
+	$ShowDepends				= 1;
+	$DaysMarkedAsNew			= $DaysMarkedAsNew = $GlobalHideLastChange = $ShowChangesLink = $ShowDescriptionLink = $ShowDownloadPortLink = $ShowHomepageLink = $ShowLastChange = $ShowMaintainedBy = $ShowPortCreationDate = $ShowPackageLink = $ShowShortDescription = 1;
+	$HideDescription			= 1;
+	$ShowEverything			= 1;
+	$ShowShortDescription	= "Y";
+	$ShowMaintainedBy			= "Y";
+	$GlobalHideLastChange	= "Y";
+	$ShowDescriptionLink		= "N";
+	$port = new Port($db);
+	$port->FetchByID($Object);
+	echo freshports_PortDetails($port, $db, $DaysMarkedAsNew, $DaysMarkedAsNew, $GlobalHideLastChange, $HideCategory, $HideDescription, $ShowChangesLink, $ShowDescriptionLink, $ShowDownloadPortLink, $ShowEverything, $ShowHomepageLink, $ShowLastChange, $ShowMaintainedBy, $ShowPortCreationDate, $ShowPackageLink, $ShowShortDescription, 1, '', 1, "N", 0);
 ?>
-		<form action="<?php echo $PostURL; ?>" method="GET" NAME=f>
+Please select the watch lists <?php echo $FromTo; ?> which this port will be <?php echo $Verb; ?>:
+<blockquote>
+		<form action="<?php echo $PostURL; ?>" method="POST" NAME=f>
 		<?php	echo freshports_WatchListDDLB($db, $UserID, '', 10, TRUE, TRUE); ?>
 		<br><br>
 		<INPUT id=submit style="WIDTH: 85px; HEIGHT: 24px" type=submit size=29 
 		   value="<?php echo $ButtonName; ?>" name=submit><br>
+		<INPUT TYPE="hidden" NAME="Origin" VALUE="<?php echo $Origin?>">
 		<INPUT TYPE="hidden" NAME="<?php echo $Action?>" VALUE="<?php echo $Object?>">
 <?php
 		if ($WatchListID) {
@@ -124,16 +153,18 @@ phpinfo();
 		}
 ?>
 		</form>
-<p>
-Port details goes there
-</p>
+</blockquote>
 </TD>
+</tr>
+</table>
   <TD VALIGN="top" WIDTH="*" ALIGN="center">
     <?
        include($_SERVER['DOCUMENT_ROOT'] . "/include/side-bars.php");
     ?>
  </TD>
 </TABLE>
+
+
 
 <TABLE WIDTH="<? echo $TableWidth; ?>" BORDER="0" ALIGN="center">
 <TR><TD>
@@ -145,24 +176,36 @@ Port details goes there
 </HTML>
 
 		<?php
-#		die("This is where I ask you what watch lists you want");
+
 		$Redirect = 0;
 	} else {
 
-	if (IsSet($_GET["remove"])) {
-		$ElementID     = AddSlashes($_GET["remove"]);
-		$WatchListsIDs = AddSlashes($_POST["watch_list_id"]);
-		if (!RemoveElementFromWatchLists($db, $UserID, $ElementID, $_GET["watch_list_id"])) {
-			echo pg_errormessage();
+	if (IsSet($_REQUEST["remove"])) {
+		$ElementID = AddSlashes($_REQUEST["remove"]);
+		if (!RemoveElementFromWatchLists($db, $UserID, $ElementID, $_REQUEST["watch_list_id"])) {
+			die("adding element failed : Please try again, and if the problem persists, please contact the webmaster.");
 			$Redirect = 0;
 		}
 	}
 
-	if (IsSet($_GET["add"])) {
+	if (IsSet($_REQUEST["add"])) {
 		$ElementID     = AddSlashes($_GET["add"]);
+#		echo 'getting stuff from post<br>';
+		$ElementID     = AddSlashes($_POST["add"]);
 		$WatchListsIDs = AddSlashes($_POST["watch_list_id"]);
-		if (!AddElementToWatchLists($db, $UserID, $ElementID, $_GET["watch_list_id"])) {
-			echo pg_errormessage();
+		/*
+		echo '$WatchListsIDs[0]=\'' . $WatchListsIDs . '\'<br>';
+		echo 'count($WatchListsIDs)=\'' . count($WatchListsIDs) . '\'<br>';
+		while (list($key, $WatchListID) = each($_REQUEST["watch_list_id"])) {
+			echo "$key = $WatchListID<br>";
+		}
+		echo '$UserID=\'' . $UserID .'\'<br>';
+		while (list($key, $WatchListID) = each($WatchListsIDs)) {
+			echo "$key = $WatchListID<br>";
+		}
+		*/
+		if (!AddElementToWatchLists($db, $UserID, $ElementID, $_REQUEST["watch_list_id"])) {
+			die("adding element failed : please contact postmaster");
 			$Redirect = 0;
 		}
 	}
@@ -170,9 +213,9 @@ Port details goes there
 
 #	echo "when done, I will return to " . $HTTP_SERVER_VARS["HTTP_REFERER"];
 	if ($Redirect) {
-		if ($origin) {
-			if ($Debug) echo "origin supplied is $origin\n<BR>";
-			$Origin = str_replace(" ", "&", $origin);
+		if ($Origin) {
+			if ($Debug) echo "Origin supplied is $Origin\n<BR>";
+			$Origin = str_replace(" ", "&", $Origin);
 		}
 
 		if ($Debug) echo "redirecting to $Origin\n<BR>";

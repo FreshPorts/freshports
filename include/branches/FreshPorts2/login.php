6251d782fa85d077aa1a6c840cca394f5b1a40fd
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2002-12-09 20:39:52 +0000

    start using new class User
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@1828 46e82423-29d8-e211-989e-002590a4cdd4

--- include/branches/FreshPorts2/login.php
+++ include/branches/FreshPorts2/login.php
@@ -1,5 +1,5 @@
 <?
-	# $Id: login.php,v 1.1.2.3 2002-05-18 08:25:09 dan Exp $
+	# $Id: login.php,v 1.1.2.4 2002-12-09 20:39:52 dan Exp $
 	#
 	# Copyright (c) 1998-2001 DVL Software Limited
 
@@ -8,7 +8,7 @@
 <form action="<?php echo $_SERVER["PHP_SELF"] . "?origin=$origin" ?>" method="POST"  name=f>
       <input type="hidden" name="custom_settings" value="1"><input type="hidden" name="LOGIN" value="1">
       <p>User ID:<br>
-      <input SIZE="15" NAME="UserID" value="<?php echo $UserID ?>"></p>
+      <input SIZE="15" NAME="UserID" value="<?php echo $User->id ?>"></p>
       <p>Password:<br>
       <input TYPE="PASSWORD" NAME="Password" VALUE = "<?php echo $Password ?>" size="20"></p>
       <p><input TYPE="submit" VALUE="Login" name=submit> &nbsp;&nbsp;&nbsp;&nbsp; <input TYPE="reset" VALUE="reset form">

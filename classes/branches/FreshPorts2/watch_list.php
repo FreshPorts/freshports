aa3e9b7b39b7bb610eae42654df0d580ebffbdf1
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2002-12-11 16:17:12 +0000

    limit watch lists to users.max_number_watch_lists
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@1875 46e82423-29d8-e211-989e-002590a4cdd4

--- classes/branches/FreshPorts2/watch_list.php
+++ classes/branches/FreshPorts2/watch_list.php
@@ -1,5 +1,5 @@
 <?
-	# $Id: watch_list.php,v 1.1.2.3 2002-12-09 20:21:46 dan Exp $
+	# $Id: watch_list.php,v 1.1.2.4 2002-12-11 16:17:12 dan Exp $
 	#
 	# Copyright (c) 1998-2001 DVL Software Limited
 	#
@@ -33,15 +33,42 @@ class WatchList {
 		$return = 0;
 
 		AddSlashes($Name);
+		
+		$query = "
+  SELECT count(*), users.max_number_watch_lists
+    FROM watch_list, users
+   WHERE users.id = $UserID
+     AND users.id = watch_list.user_id
+GROUP BY users.max_number_watch_lists";
 
-		$NextValue = freshports_GetNextValue($Sequence_Watch_List_ID, $this->dbh);
-
-		$query  = "insert into watch_list (id, user_id, name) values ($NextValue, $UserID, '$Name')";
-		$result = pg_query($this->dbh, $query);
-
-		# that worked and we updated exactly one row
-		if ($result && pg_affected_rows($result) == 1) {
-			$return = $NextValue;
+		$this->LocalResult = pg_query($this->dbh, $query);
+		if ($this->LocalResult) {
+			$numrows = pg_numrows($this->LocalResult);
+			if ($numrows == 1) {
+				$myrow = pg_fetch_array($this->LocalResult, 0);
+				$Count = $myrow[0];
+				$Max   = $myrow[1];
+				if ($Count < $Max) {
+					$NextValue = freshports_GetNextValue($Sequence_Watch_List_ID, $this->dbh);
+			
+					$query  = "insert into watch_list (id, user_id, name) values ($NextValue, $UserID, '$Name')";
+					$result = pg_query($this->dbh, $query);
+			
+					# that worked and we updated exactly one row
+					if ($result && pg_affected_rows($result) == 1) {
+						$return = $NextValue;
+					}
+				} else {
+					syslog(LOG_NOTICE, "You already have $Count watch lists.  If you want more than $Max watch lists, please contact the postmaster. UserID='$UserID'");
+					die("You already have $Count watch lists.  If you want more than $Max watch lists, please contact the postmaster.");
+				}
+			} else {
+				syslog(LOG_ERR, "Could not find watch list count for user $UserID - " . $_SERVER['PHP_SELF']);
+				die("I couldn't find your watch list details... sorry");
+			}
+		} else {
+			syslog(LOG_ERR, "Error finding watch list count for user $UserID - " . $_SERVER['PHP_SELF'] . ' ' . pg_last_error());
+			die('Error finding watch list count for user');
 		}
 
 		return $return;

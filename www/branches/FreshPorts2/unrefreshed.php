f0dcc636c2c751dfaa6b4f01f42617b15dfd2dc6
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2003-02-25 19:09:23 +0000

    This seems to work now.
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@2014 46e82423-29d8-e211-989e-002590a4cdd4

--- www/branches/FreshPorts2/unrefreshed.php
+++ www/branches/FreshPorts2/unrefreshed.php
@@ -1,5 +1,5 @@
 <?
-	# $Id: unrefreshed.php,v 1.1.2.1 2003-02-25 15:25:15 dan Exp $
+	# $Id: unrefreshed.php,v 1.1.2.2 2003-02-25 19:09:23 dan Exp $
 	#
 	# Copyright (c) 1998-2001 DVL Software Limited
 
@@ -39,16 +39,16 @@ function freshports_DisplayUnrefreshedPorts($dbh) {
 <FORM ACTION="<? echo $_SERVER["PHP_SELF"]; ?>" method="POST">
 <TABLE WIDTH="100%" border="1" CELLSPACING="0" CELLPADDING="8">
 <tr>
-<td><b>Port ID</b></td>
-<td><b>Port Name</b></td>
-<td><b>Category</b></td>
-<td><b>Category ID</b></td>
-<td><b>Commit Log ID</b></td>
-<td><b>Monitor</b></td>
-<td><b>Ignore</b></td>
-<td><b>Reset</b></td>
-<td><b>Reason</b></td>
-<td><b>Date Ignored</b></td>
+<td valign="top" align="center"><b>Port ID</b></td>
+<td valign="top" align="center"><b>Port Name</b></td>
+<td valign="top" align="center"><b>Category</b></td>
+<td valign="top" align="center"><b>Category ID</b></td>
+<td valign="top" align="center"><b>Commit Log ID</b></td>
+<td valign="top" align="center"><b>Monitor</b></td>
+<td valign="top" align="center"><b>Ignore</b></td>
+<td valign="top" align="center"><b>Mark as Refreshed</b></td>
+<td valign="top" align="center"><b>Reason</b></td>
+<td valign="top" align="center"><b>Date Ignored</b></td>
 <?php
 		
 		for ($i = 0; $i < $NumRows; $i++) {
@@ -62,16 +62,16 @@ function freshports_DisplayUnrefreshedPorts($dbh) {
 <INPUT NAME="port_id[]"        TYPE="hidden" value="<?php echo $Ports->port_id;       ?>">
 <INPUT NAME="commit_log_id[]"  TYPE="hidden" value="<?php echo $Ports->commit_log_id; ?>"><?php
 			echo $Ports->port_id . '</td>' . "\n";
-			echo '<td valign="top"><a href="/' . $Ports->category_name . '/' . $Ports->port_name . '/">' . $Ports->port_name  . '</a></td>' . "\n";
-			echo '<td valign="top">'           . $Ports->category_id   . '</td>';
+			echo '<td valign="top" nowrap><a href="/' . $Ports->category_name . '/' . $Ports->port_name . '/">' . $Ports->port_name  . '</a></td>' . "\n";
+			echo '<td valign="top">'                  . $Ports->category_id   . '</td>';
 			echo '<td valign="top"><a href="/' . $Ports->category_name . '/">' . $Ports->category_name  . '</a></td>' . "\n";
 			echo '<td  valign="top"nowrap>'    . $Ports->commit_log_id . ' '. freshports_Commit_Link($Ports->message_id) . ' ' . freshports_Email_Link($Ports->message_id) . '</td>' . "\n";
 ?>
-<td valign="top"><INPUT NAME="action[<?php echo $ID;?>]" TYPE="RADIO" VALUE="Monitor"<?php if (!IsSet($Ports->date_ignored)) echo " CHECKED"; ?>></td>
-<td valign="top"><INPUT NAME="action[<?php echo $ID;?>]" TYPE="RADIO" VALUE="Ignore" <?php if ( IsSet($Ports->date_ignored)) echo " CHECKED"; ?>></td>
-<td valign="top"><INPUT NAME="action[<?php echo $ID;?>]" TYPE="RADIO" VALUE="Reset"></td>
+<td valign="top" align="center" ><INPUT NAME="action[<?php echo $ID;?>]" TYPE="RADIO" VALUE="Monitor"<?php if (!IsSet($Ports->date_ignored)) echo " CHECKED"; ?>></td>
+<td valign="top" align="center" ><INPUT NAME="action[<?php echo $ID;?>]" TYPE="RADIO" VALUE="Ignore" <?php if ( IsSet($Ports->date_ignored)) echo " CHECKED"; ?>></td>
+<td valign="top" align="center" ><INPUT NAME="action[<?php echo $ID;?>]" TYPE="RADIO" VALUE="Reset"></td>
 <td valign="top"><INPUT NAME="reason[<?php echo $ID;?>]" TYPE="text" SIZE="18" VALUE="<?php echo $Ports->reason; ?>"></td>
-<td valign="top" nowrap><INPUT NAME="date[<?php   echo $ID;?>]" TYPE="hidden" VALUE="<?php echo freshports_IfNull($Ports->date_ignored); ?>"><?php echo freshports_IfNull($Ports->date_ignored); ?></td>
+<td valign="top" nowrap><INPUT NAME="date[<?php   echo $ID;?>]" TYPE="hidden"  VALUE="<?php echo $Ports->date_ignored; ?>"><?php echo freshports_IfNull($Ports->date_ignored); ?></td>
 </tr>
 <?php
 		}
@@ -93,21 +93,71 @@ if (IsSet($_POST['port_id'])) {
 	# OK, time to update things!
 	#
    require_once($_SERVER['DOCUMENT_ROOT'] . '/../classes/commit_log_ports.php');
+   require_once($_SERVER['DOCUMENT_ROOT'] . '/../classes/commit_log_ports_ignore.php');
+   
+  	pg_exec ($db, 'BEGIN');
+  	$CommitOrRollback = 'COMMIT';
 
-	$CommitLogPort = new Commit_Log_Ports($db);
+
+	$CommitLogPort       = new Commit_Log_Ports       ($db);
+	$CommitLogPortIgnore = new Commit_Log_Ports_Ignore($db);
+	
 	while (list($key, $port_id) = each($_POST['port_id'])) {
 		$commit_log_id = $_POST['commit_log_id'][$key];
+
+		$ID = $commit_log_id . '_' . $port_id;
+		
+		$action        = $_POST['action'][$ID];
+		$reason        = $_POST['reason'][$ID];
+		$date          = $_POST['date'][$ID];
+
+/*
 		echo "\$key='$key' :: \$port_id=$port_id";
 		echo " :: commit_log_id='$commit_log_id'";
 
-		$ID = $commit_log_id . '_' . $port_id;
 		echo " :: \$ID='$ID'";
-		echo " :: action='"        . $_POST['action'][$ID] . "'";
-		echo " :: reason='"        . $_POST['reason'][$ID] . "'";
-		echo " :: date  ='"        . $_POST['date']  [$ID] . "'";
+		echo " :: action='"        . $action . "'";
+		echo " :: reason='"        . $reason . "'";
+		echo " :: date  ='"        . $date   . "'";
 		echo "<br>\n";
+*/
+
+		$CommitLogPort->CommitLogIDSet      ($commit_log_id);
+		$CommitLogPort->PortIDSet           ($port_id);
+
+		$CommitLogPortIgnore->CommitLogIDSet($commit_log_id);
+		$CommitLogPortIgnore->PortIDSet     ($port_id);
+
+		switch ($action) {
+			case 'Monitor':
+				# remove the entry from commit_log_port_ignore if one exists
+				if (IsSet($date) && chr($date) != 0) {
+					$numrows = $CommitLogPortIgnore->delete();
+				}
+				break;
+
+			case 'Ignore':
+				if (!IsSet($reason) || chr($reason) == 0) {
+					$reason = 'none given';
+				}
+				$CommitLogPortIgnore->ReasonSet($reason);
+				$numrows = $CommitLogPortIgnore->insert();
+
+				break;
+
+			case 'Reset':
+				$numrows = $CommitLogPort->NeedsRefreshClear();
+				break;
+
+			default:
+				die("unknown action found: '$action'");
+		}
+		
+		if ($numrows != 1) {
+			$CommitOrRollback = 'ROLLBACK';
+		}
 	}
-	
+	pg_exec ($db, $CommitOrRollback);
 }
 
 ?>

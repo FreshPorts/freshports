9fed983479fc10301f9ef39cac316616c27ccebd
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2003-04-28 20:45:42 +0000

    Let's bring this up to speed too
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@2220 46e82423-29d8-e211-989e-002590a4cdd4

--- www/branches/FreshPorts2/backend/sidebar.php
+++ www/branches/FreshPorts2/backend/sidebar.php
@@ -1,9 +1,11 @@
 <?php
 	#
-	# $Id: sidebar.php,v 1.1.2.14 2003-04-27 14:48:17 dan Exp $
+	# $Id: sidebar.php,v 1.1.2.15 2003-04-28 20:45:42 dan Exp $
 	#
 	# Copyright (c) 1998-2003 DVL Software Limited
 	#
+
+	DEFINE('MAX_PORTS', 40);
 
 	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/common.php');
 	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/freshports.php');
@@ -54,9 +56,58 @@
 
 <?
 
-	$sql = "select * from commits_latest_ports order by commit_date_raw desc, category, port";
+$sql = "
+SELECT PEC.*,
+       security_notice.id  AS security_notice_id
+FROM (
+SELECT PORTELEMENT.*,
+       categories.name AS category
+FROM (
+SELECT LCPPORTS.*,
+       element.name    AS port,
+       element.status  AS status
 
-	$sql .= " limit 40";
+FROM (
+SELECT LCPCLLCP.*,
+       ports.forbidden,
+       ports.broken,
+       ports.element_id                     AS element_id,
+       CASE when clp_version  IS NULL then ports.version  else clp_version  END as version,
+       CASE when clp_revision IS NULL then ports.revision else clp_revision END AS revision,
+       ports.version                        AS ports_version,
+       ports.revision                       AS ports_revision,
+       date_part('epoch', ports.date_added) AS date_added,
+       ports.short_description              AS short_description,
+       ports.category_id
+FROM (
+ SELECT LCPCL.*, 
+         port_id,
+         commit_log_ports.port_version  AS clp_version,
+         commit_log_ports.port_revision AS clp_revision,
+         commit_log_ports.needs_refresh AS needs_refresh
+    FROM 
+   (SELECT commit_log.id     AS commit_log_id, 
+           commit_date       AS commit_date_raw,
+           message_subject,
+           message_id,
+           committer,
+           description       AS commit_description,
+           to_char(commit_log.commit_date - SystemTimeAdjust(), 'DD Mon YYYY')  AS commit_date,
+           to_char(commit_log.commit_date - SystemTimeAdjust(), 'HH24:MI')      AS commit_time,
+           encoding_losses
+     FROM commit_log JOIN
+               (SELECT latest_commits_ports.commit_log_id
+                   FROM latest_commits_ports
+               ORDER BY latest_commits_ports.commit_date DESC
+                 LIMIT $MaxNumberOfPorts) AS LCP
+           ON commit_log.id = LCP.commit_log_id) AS LCPCL JOIN commit_log_ports
+                         ON commit_log_ports.commit_log_id = LCPCL.commit_log_id
+                         AND commit_log_ports.commit_log_id > latest_commits_ports_anchor()) AS LCPCLLCP JOIN ports
+on LCPCLLCP.port_id = ports.id) AS LCPPORTS JOIN element
+on LCPPORTS.element_id = element.id) AS PORTELEMENT JOIN categories
+on PORTELEMENT.category_id = categories.id) AS PEC LEFT OUTER JOIN security_notice
+ON PEC.commit_log_id = security_notice.commit_log_id
+order by commit_date_raw desc, category, port ";
 
 	if ($Debug) {
 		echo $sql;

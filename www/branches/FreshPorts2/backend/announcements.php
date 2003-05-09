b5918a48752d64a7839288738938e0dcbfbd97c8
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2003-05-09 21:33:00 +0000

    Beginnings of announcement maintenance
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@2272 46e82423-29d8-e211-989e-002590a4cdd4

--- www/branches/FreshPorts2/backend/announcements.php
+++ www/branches/FreshPorts2/backend/announcements.php
@@ -1,6 +1,6 @@
 <?php
 	#
-	# $Id: announcements.php,v 1.1.2.1 2003-05-09 19:42:42 dan Exp $
+	# $Id: announcements.php,v 1.1.2.2 2003-05-09 21:33:00 dan Exp $
 	#
 	# Copyright (c) 1998-2003 DVL Software Limited
 	#
@@ -33,15 +33,31 @@ if (!$User->IsTaskAllowed(FRESHPORTS_TASKS_ANNOUNCEMENTS_MAINTAIN)) {
 	die("I'm sorry, but you're not allowed to be here.  The police have been notified.  Please leave now.");
 }
 
-if ($_REQUEST['Add']) {
-   $Debug = 0;
+$Announcement = new Announcement($db);
 
-	$Announcement = new Announcement($db);
-	$Announcement->TextSet     ($_REQUEST['announcement']);
-	$Announcement->StartDateSet($_REQUEST['start_date']);
-	$Announcement->EndDateSet  ($_REQUEST['end_date']);
+phpinfo();
+die();
+if (IsSet($_REQUEST['Add'])) {
+	$Announcement->TextSet     (AddSlashes($_REQUEST['announcement']));
+	$Announcement->StartDateSet(AddSlashes($_REQUEST['start_date']));
+	$Announcement->EndDateSet  (AddSlashes($_REQUEST['end_date']));
 
 	$Announcement->Insert();
+}
+
+if (IsSet($_REQUEST['Update'])) {
+	$Announcement->TextSet     (AddSlashes($_REQUEST['announcement']));
+	$Announcement->StartDateSet(AddSlashes($_REQUEST['start_date']));
+	$Announcement->EndDateSet  (AddSlashes($_REQUEST['end_date']));
+
+	$Announcement->Update();
+}
+
+# create another new one so after a save, we don't redisplay the old one.
+$Announcement = new Announcement($db);
+
+if (IsSet($_REQUEST['id'])) {
+	$Announcement->Fetch(AddSlashes($_REQUEST['id']));
 }
 
 	#echo '<br>the page size is ' . $page_size . ' : ' . $email;
@@ -72,9 +88,6 @@ echo '<p>If you need help, please post a message on the forum. </p>
  </TABLE>
 <br>';
 }
-if ($AccountModified) {
-   echo "Your account details were successfully updated.";
-} else {
 
 echo '<TABLE CELLPADDING="1" CELLSPACING="3" BORDER="0" BGCOLOR="#AD0040" WIDTH="100%">
 <TR>
@@ -95,22 +108,26 @@ $HTML .= '<tr>'  . "\n";
 
 $HTML .= '<td>'  . "\n";
 $HTML .= '<TEXTAREA NAME="announcement" ROWS="10" COLS="60">'          . "\n";
-$HTML .= $Announcement->$title;
+$HTML .= $Announcement->TextGet();
 $HTML .= '</TEXTAREA>';
 $HTML .= '</td>'  . "\n";
 
 $HTML .= '<td valign="top">'  . "\n";
-$HTML .= '<INPUT id="start_date" name="start_date" value="' . $Announcement->start_date . '" size=10>' . "\n";
+$HTML .= '<INPUT id="start_date" name="start_date" value="' . $Announcement->StartDateGet() . '" size=10>' . "\n";
 $HTML .= '</td>'  . "\n";
 
 $HTML .= '<td valign="top">'  . "\n";
-$HTML .= '<INPUT id="end_date"   name="end_date"   value="' . $Announcement->end_date   . '" size=10>' . "\n";
+$HTML .= '<INPUT id="end_date"   name="end_date"   value="' . $Announcement->EndDateGet()   . '" size=10>' . "\n";
 $HTML .= '</td>'  . "\n";
 
 $HTML .= '</tr>'  . "\n";
 
+$ControlName  = IsSet($_REQUEST['id']) ? 'update' : 'add';
+$ControlValue = IsSet($_REQUEST['id']) ? 'Update' : 'Add';
+
 $HTML .= '<tr><td colspan="3">'  . "\n";
-$HTML .= '<div align="center"><INPUT id="add" style="WIDTH: 85px; HEIGHT: 24px" type="submit" size="29" value="Add" name="Add"></div>' . "\n";
+$HTML .= '<div align="center"><INPUT id="' . $ControlName . '" style="WIDTH: 85px; HEIGHT: 24px" type="submit" size="29" value="' . $ControlValue . '"';
+$HTML .= ' name="' . $ControlName . '"></div>' . "\n";
 $HTML .= '</tr>'  . "\n";
 
 $HTML .= '</table>' . "\n";
@@ -122,7 +139,30 @@ echo $HTML;
 echo "<p></blockquote></TD>
 </TR>
 </TABLE>";
+
+$NumRows = $Announcement->FetchAllActive();
+
+if ($NumRows > 0) {
+	echo '<blockquote>' . "\n";
+	echo '<h2>Existing Announcements</h2>' . "\n";
+	echo '<table cellpadding="4" cellspacing="0" border="1">' . "\n";
+	echo '<tr><td><b>Announcement Text</b></td><td><b>Start Date</b></td><td><b>End Date</b></td><td><b>Edit</b></td></tr>' . "\n";
+	for ($i = 0; $i < $NumRows; $i++) {
+		$Announcement->FetchNth($i);
+		echo '<tr>' . "\n";
+		echo '<td>' . $Announcement->TextGet()      . '</td>';
+		echo '<td>' . ($Announcement->StartDateGet() != '' ? $Announcement->StartDateGet() : '&nbsp') . '</td>';
+		echo '<td>' . ($Announcement->EndDateGet()   != '' ? $Announcement->EndDateGet()   : '&nbsp') . '</td>';
+		echo '<td><a href="' . $_SERVER['PHP_SELF']  . '?id=' . $Announcement->IDGet() . '">Edit</a></td>';
+      echo '</tr>' . "\n";
+	}
+	echo '</table>' . "\n";
+	echo '</blockquote>' . "\n";
+} else {
+	echo 'There are no active announcements';
 }
+
+
 
 ?>
 

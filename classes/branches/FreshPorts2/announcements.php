c89a00497d70fccfa3387158bb1fe788b0d6a432
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2003-05-09 21:32:32 +0000

    add FetchNth, FetchAllActive, FetchUpdateID, and GetPopulateValues functions
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@2271 46e82423-29d8-e211-989e-002590a4cdd4

--- classes/branches/FreshPorts2/announcements.php
+++ classes/branches/FreshPorts2/announcements.php
@@ -1,6 +1,6 @@
 <?php
 	#
-	# $Id: announcements.php,v 1.1.2.1 2003-05-09 19:39:55 dan Exp $
+	# $Id: announcements.php,v 1.1.2.2 2003-05-09 21:32:32 dan Exp $
 	#
 	# Copyright (c) 1998-2003 DVL Software Limited
 	#
@@ -10,6 +10,7 @@
 class Announcement {
 
 	var $dbh;
+	var $LocalResult;
 
 	var $id;
 	var $text;
@@ -19,7 +20,11 @@ class Announcement {
 	function Announcement($dbh) {
 		$this->dbh = $dbh;
 	}
-	
+
+	function IDGet() {
+		return $this->id;
+	}
+
 	function TextSet($text) {
 		$this->text = AddSlashes($text);
 	}
@@ -42,6 +47,13 @@ class Announcement {
 
 	function EndDateGet() {
 		return $this->end_date;
+	}
+
+	function PopulateValues($myrow) {
+		$this->id			= $myrow["id"];
+		$this->text			= $myrow["text"];
+		$this->start_date	= $myrow["start_date"];
+		$this->end_date	= $myrow["end_date"];
 	}
 
 	function Delete() {
@@ -96,6 +108,96 @@ DELETE from announcements
 		
 	}
 	
+	function Update() {
+		# delete the ignore entry for this commit/port combination
+
+		$sql = "UPDATE announcements set text = '" . $this->text . "', start_date = ";
+
+		if ($this->start_date != '') {
+			$sql .= ', start_date';
+		} else {
+			$sql .= 'NULL';
+		}
+
+		$sql .= ", end_date = '";		
+		if ($this->end_date != '') {
+			$sql .= ', end_date';
+		} else {
+			$sql .= 'NULL';
+		}
+
+		$this->result = pg_exec($this->dbh, $sql);
+		if (!$this->result) {
+			echo pg_errormessage() . " $sql";
+		}
+		$numrows = pg_affected_rows($this->result);
+
+		return $numrows;
+		
+	}
+	
+	function Fetch($id) {
+
+		$sql = "
+SELECT *
+  FROM announcements
+ WHERE id = $id";
+
+#		echo "sql = '<pre>$sql</pre>'<BR>";
+
+		$result = pg_exec($this->dbh, $sql);
+		if ($result) {
+			$numrows = pg_numrows($result);
+			if ($numrows == 1) {
+				if ($Debug) echo "fetched by ID succeeded<BR>";
+				$myrow = pg_fetch_array ($result, 0);
+				$this->PopulateValues($myrow);
+			}
+		}
+
+		return $this->id;
+	}
+
+	function FetchAllActive() {
+		$sql = "
+		SELECT id,
+             text,
+             start_date,
+             end_date
+        FROM announcements
+       WHERE (start_date <= current_date OR start_date IS NULL)
+         AND (end_date   >= current_date OR end_date   IS NULL)";
+
+#		echo '<pre>' . $sql . '</pre>';
+
+		if ($Debug)	echo "commits::Fetch sql = '$sql'<BR>";
+
+		$this->LocalResult = pg_exec($this->dbh, $sql);
+		if ($this->LocalResult) {
+			$numrows = pg_numrows($this->LocalResult);
+#			echo "That would give us $numrows rows";
+		} else {
+			$numrows = -1;
+			echo 'pg_exec failed: ' . $sql;
+		}
+
+		return $numrows;
+	}
+
+	function FetchNth($N) {
+		#
+		# call Fetch first.
+		# then call this function N times, where N is the number
+		# returned by Fetch
+		#
+
+#		echo "fetching row $N<br>";
+
+		$myrow = pg_fetch_array($this->LocalResult, $N);
+		$this->PopulateValues($myrow);
+
+		return $this->id;
+	}
 	
 
 }

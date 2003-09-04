5518320b229497bbcf5c5cfe47ef1c4e3d097bea
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2003-09-04 15:15:06 +0000

    We don't need this in this repo any more.  Phorum is now standalone.
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@2389 46e82423-29d8-e211-989e-002590a4cdd4

--- www/branches/FreshPorts2/phorum-3.3.2a/WoCKer921/actions/activate.php
+++
@@ -1,8 +0,0 @@
-<?php check_security(); ?>
-<?php
-  // activate the current forum
-  $sSQL="Update ".$pho_main." set active=1 where id=$num";
-  $q->query($DB, $sSQL);
-  $ForumActive=1;
-  writefile($num, true);
-?>
--- www/branches/FreshPorts2/phorum-3.3.2a/WoCKer921/actions/stop.php
+++
@@ -1,6 +0,0 @@
-<?php check_security(); ?>
-<?php
-  $PHORUM["started"]=0;
-  writefile();
-  QueMessage("Phorum has been stopped.");
-?>

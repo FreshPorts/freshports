dfe8f319ae0f2bd9ade07dc62118e57f2a866952
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2003-09-09 19:24:00 +0000

    The HTMLify code is now in another file.  Remove it from here, and include it when needed.
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@2417 46e82423-29d8-e211-989e-002590a4cdd4

--- include/branches/FreshPorts2/freshports.php
+++ include/branches/FreshPorts2/freshports.php
@@ -1,6 +1,6 @@
 <?php
 	#
-	# $Id: freshports.php,v 1.4.2.152 2003-09-08 13:53:02 dan Exp $
+	# $Id: freshports.php,v 1.4.2.153 2003-09-09 19:24:00 dan Exp $
 	#
 	# Copyright (c) 1998-2003 DVL Software Limited
 	#
@@ -31,12 +31,12 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/../classes/watchnotice.php');
 function freshports_IndexFollow($URI) {
 	$NOINDEX{'/index.php'}				= 1;
 	$NOINDEX{'/date.php'}				= 1;
-	$NOINDEX{'/ports-deleted.php'}	= 1;
+	$NOINDEX{'/ports-deleted.php'}		= 1;
 	$NOINDEX{'/ports-new.php'}			= 1;
 	$NOINDEX{'/ports-forbidden.php'}	= 1;
 	$NOFOLLOW{'/date.php'}				= 1;
 	$NOFOLLOW{'/graphs.php'}			= 1;
-	$NOFOLLOW{'/ports-deleted.php'}	= 1;
+	$NOFOLLOW{'/ports-deleted.php'}		= 1;
 
 	# well, OK, so it may not be a URI... but it's close
 
@@ -647,6 +647,8 @@ function freshports_PortDetails($port, $db, $ShowDeletedDate, $DaysMarkedAsNew, 
 	GLOBAL $User;
 	GLOBAL $freshports_CommitMsgMaxNumOfLinesToShow;
 
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/htmlify.php');
+
 	$MarkedAsNew = "N";
 	$HTML  = "<DL>\n";
 
@@ -937,109 +939,6 @@ function FormatTime($Time, $Adjustment, $Format) {
 	return date($Format, strtotime($Time) + $Adjustment);
 }
 
-#
-# The code below was donated by Steve Kacsmark <stevek@guide.chi.il.us>.
-# With modifications by Marcin Gryszkalis <mgryszkalis@cerint.pl>.
-#
-
-
-function freshports_IsEmailValid($email) {
-	# see also convertMail
-	if (eregi("^[a-z0-9\._+-]+@[a-z0-9\._-]+$", $email)) {
-		return TRUE;
-	} else {
-		return FALSE;
-	}
-}
-
-
-
-function pr2link($Arr) {
-	return preg_replace("/((\w+\/)?\d+)/", 
-					"<A HREF=\"http://www.FreeBSD.org/cgi/query-pr.cgi?pr=\\1\">\\1</A>",
-					$Arr[0]);  
-}
-
-function mail2link($Arr) {
-	#
-	# in an attempt to reduce spam, encode the mailto
-	# so the spambots get rubbish, but it works OK in
-	# the browser.
-	#
-
-	$addr     = $Arr[0];
-	$new_addr = "";
-
-	for ($i=0; $i<strlen($addr); $i++) {
-		$new_addr .= ("&#".ord($addr[$i]).";");
-	}
-
-	$addr = "<A HREF=\"" . MAILTO . ":$new_addr\">$new_addr</A>";
-
-	return $addr;
-}
-
-function url2link($Arr) {
-	#
-	# URLs will be truncated if they are too long. But only
-	# the visible part
-	#
-	$html  = $Arr[1];
-	$vhtml = $html;
-
-	$html  = preg_replace("/@/", "&#64", $html);
-	$vhtml = preg_replace("/@/", "&#64", $vhtml);
-	
-	return "<A HREF=\"$html\">$vhtml</A>" . $Arr[3];
-}
-
-function url_shorten($Arr) {
-	#
-	# URLs will be truncated if they are too long. But only
-	# the visible part
-	#
-	$html = $Arr[1];
-#	syslog(LOG_NOTICE, "start");
-#	syslog(LOG_NOTICE, "0 - $Arr[0]");
-#	syslog(LOG_NOTICE, "1 - $Arr[1]");
-#	syslog(LOG_NOTICE, "2 - $Arr[2]");
-#	syslog(LOG_NOTICE, "3 - $Arr[3]");
-#	syslog(LOG_NOTICE, "4 - $Arr[4]");
-#	syslog(LOG_NOTICE, "5 - $Arr[5]");
-#	syslog(LOG_NOTICE, "6 - $Arr[6]");
-#	syslog(LOG_NOTICE, "finish");
-
-	$URL = $Arr[5];
-	if (URL2LINK_CUTOFF_LEVEL > 0 && strlen($URL) > URL2LINK_CUTOFF_LEVEL) {
-		$URL = substr($URL, 0, URL2LINK_CUTOFF_LEVEL - 5) . "(...)";
-	}
-
-	return $Arr[1] . '">' . $URL . '</a>';
-}
-
-function htmlify($String) {
-
-#
-# URLs to test with: http://www.freshports.org/commit.php?message_id=200206232029.g5NKT1O13181@freefall.freebsd.org
-#
-	$del_t = array("&quot;","&#34;","&gt;","&#62;","\/\.\s","\)","'","\s","$");
-	$delimiters = "(".join("|",$del_t).")";
-
-	$String = preg_replace_callback("/((http|ftp|https):\/\/.*?)($delimiters)/i",                    'url2link',    $String);
-	$String = preg_replace_callback("/(<a href=(\"|')(http|ftp|https):\/\/.*?)(\">|'>)(.*?)<\/a>/i", 'url_shorten', $String);
-	$String = preg_replace_callback("/([\w+=\-.!]+@[\w\-]+(\.[\w\-]+)+)/",                           'mail2link',   $String);
-	$String = preg_replace_callback("/(\bPR[:\#]?)\s*(((\w+\/)?\d+)(,?[\s\n]*((\w+\/)?\d+))*)/",     'pr2link',     $String);
- 
-	return $String;
-}
-
-
-#
-# The code above was donated by Steve Kacsmark <stevek@guide.chi.il.us>.
-# With modifications by Marcin Gryszkalis <mgryszkalis@cerint.pl>.
-#
-
-
 function freshports_PortCommitsHeader($port) {
 	# print the header for the commits for a port
 
@@ -1183,6 +1082,9 @@ function freshports_Head($string, $n) {
 }
 
 function freshports_PortDescriptionPrint($description, $encoding_losses, $maxnumlines=0, $URL='') {
+
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/htmlify.php');
+
 	$shortened = freshports_Head($description, $maxnumlines);
 	$HTML  = '<PRE CLASS="code">';
 

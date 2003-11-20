9b04989bbb1ecc21c6e2d21079d0aa9b4642fa6a
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2003-11-20 14:27:12 +0000

    Start using new classes/commit_record.php
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@2585 46e82423-29d8-e211-989e-002590a4cdd4

--- www/branches/FreshPorts2/index.php
+++ www/branches/FreshPorts2/index.php
@@ -1,6 +1,6 @@
 <?php
 	#
-	# $Id: index.php,v 1.1.2.84 2003-10-06 17:09:41 dan Exp $
+	# $Id: index.php,v 1.1.2.85 2003-11-20 14:27:12 dan Exp $
 	#
 	# Copyright (c) 1998-2003 DVL Software Limited
 	#
@@ -11,7 +11,6 @@
 
 	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/getvalues.php');
 
-	require_once($_SERVER['DOCUMENT_ROOT'] . '/../classes/commit_record.php');
 	freshports_Start($FreshPortsSlogan,
 					$FreshPortsName . ' - new ports, applications',
 					'FreeBSD, index, applications, ports');
@@ -86,42 +85,12 @@ if (Is_Numeric($dailysummary)) {
 }
 
 
-$database=$db;
-if ($database) {
-
-$sql = "select * from LatestCommits($MaxNumberOfPorts, ";
-if ($User->id) {
-	$sql .= $User->id;
-} else {
-	$sql .= 0;
-}
-$sql .= ');';
-
-if ($Debug) echo "\n<pre>sql=$sql</pre>\n";
-
-
-$result = pg_exec($database, $sql);
-if ($result) {
-	$NumRows = pg_numrows($result);
-	if ($Debug) echo "Number of rows = $NumRows<br>";
-	if ($NumRows) { 
-	
-		$i=0;
-		$GlobalHideLastChange = "N";
-		for ($i = 0; $i < $NumRows; $i++) {
-			$myrow = pg_fetch_array ($result, $i);
-			$mycommit = new CommitRecord($database);
-			$mycommit->PopulateValues($myrow);
-			$commits[$i] = $mycommit;
-		}
-	
-		$LastDate = '';
+if ($db) {
 ?>
-
 <TR><TD VALIGN="top" WIDTH="100%">
 <TABLE WIDTH="100%" border="1" CELLSPACING="0" CELLPADDING="8">
 <TR>
-		<? echo freshports_PageBannerText("$MaxNumberOfPorts most recently changed ports", 3); ?>
+<? echo freshports_PageBannerText("$MaxNumberOfPorts most recently changed ports", 3); ?>
         <? //echo ($StartAt + 1) . " - " . ($StartAt + $MaxNumberOfPorts) ?>
 </TR>
 <TR><TD>
@@ -131,152 +100,21 @@ ports. A port is marked as new for 10 days.
 </P>
 
 </TD></TR>
+<?php
+	if ($User->id == '') {
+		readfile($_SERVER['DOCUMENT_ROOT'] . '/../caching/cache/index.html');
+	} else {
+		require_once($_SERVER['DOCUMENT_ROOT'] . '/../classes/latest_commits.php');
 
-<?
-				unset($ThisCommitLogID);
-				for ($i = 0; $i < $NumRows; $i++) {
-					$HTML = "";
-					$mycommit = $commits[$i];
-					$ThisCommitLogID = $mycommit->commit_log_id;
+		$LatestCommits = new LatestCommits($db, $MaxNumberOfPorts);
 
-					if ($LastDate <> $mycommit->commit_date) {
-						$LastDate = $mycommit->commit_date;
-						$HTML .= '<TR><TD COLSPAN="3" BGCOLOR="#AD0040" HEIGHT="0">' . "\n";
-						$HTML .= '   <FONT COLOR="#FFFFFF"><BIG>' . FormatTime($mycommit->commit_date, 0, "D, j M Y") . '</BIG></FONT>' . "\n";
-						$HTML .= '</TD></TR>' . "\n\n";
-					}
+		echo $LatestCommits->HTML;
+	}
 
-					$j = $i;
-
-					$HTML .= "<TR><TD>\n";
-
-					// OK, while we have the log change log, let's put the port details here.
-
-					# count the number of ports in this commit
-					$NumberOfPortsInThisCommit = 0;
-					$MaxNumberPortsToShow      = 10;
-					while ($j < $NumRows && $commits[$j]->commit_log_id == $ThisCommitLogID) {
-#echo "in NumberOfPortsInThisCommit loop $i, $j<br>";
-						$NumberOfPortsInThisCommit++;
-						$mycommit = $commits[$j];
-
-						if ($NumberOfPortsInThisCommit == 1) {
-							GLOBAL $freshports_mail_archive;
-
-							$HTML .= '<SMALL>';
-							$HTML .= '[ ' . $mycommit->commit_time . ' ' . freshports_CommitterEmailLink($mycommit->committer) . ' ]';
-							$HTML .= '</SMALL>';
-							$HTML .= '&nbsp;';
-							$HTML .= freshports_Email_Link($mycommit->message_id);
-
-							if ($mycommit->EncodingLosses()) {
-								$HTML .= '&nbsp;' . freshports_Encoding_Errors();
-							}
-
-							if (IsSet($mycommit->security_notice_id)) {
-								$HTML .= ' <a href="/security-notice.php?message_id=' . $mycommit->message_id . '">' . freshports_Security_Icon() . '</a>';
-							}
-
-						}
-
-						if ($NumberOfPortsInThisCommit <= $MaxNumberPortsToShow) {
-
-							$HTML .= "<BR>\n";
-
-							if (IsSet($mycommit->category) || $mycommit->category != '') {
-								$HTML .= '<BIG><B>';
-								$HTML .= '<A HREF="/' . $mycommit->category . '/' . $mycommit->port . '/">';
-								$HTML .= $mycommit->port;
-								$HTML .= '</A>';
-			
-								if (strlen($mycommit->version) > 0) {
-									$HTML .= ' ' . $mycommit->version;
-									if (strlen($mycommit->revision) > 0 && $mycommit->revision != "0") {
-							    		$HTML .= '-' . $mycommit->revision;
-									}
-								}
-
-								$HTML .= "</B></BIG>\n";
-
-								$HTML .= '<A HREF="/' . $mycommit->category . '/">';
-								$HTML .= $mycommit->category. "</A>";
-								$HTML .= '&nbsp;';
-
-								if ($User->id) {
-									if ($mycommit->watch) {
-										$HTML .= ' '. freshports_Watch_Link_Remove($User->watch_list_add_remove, $mycommit->watch, $mycommit->element_id) . ' ';
-									} else {
-										$HTML .= ' '. freshports_Watch_Link_Add   ($User->watch_list_add_remove, $mycommit->watch, $mycommit->element_id) . ' ';
-									}
-								}
-
-								// indicate if this port has been removed from cvs
-								if ($mycommit->status == "D") {
-									$HTML .= " " . freshports_Deleted_Icon() . "\n";
-								}
-
-								// indicate if this port needs refreshing from CVS
-								if ($mycommit->needs_refresh) {
-								$HTML .= " " . freshports_Refresh_Icon() . "\n";
-								}
-
-								if ($mycommit->date_added > Time() - 3600 * 24 * $DaysMarkedAsNew) {
-									$MarkedAsNew = "Y";
-									$HTML .= freshports_New_Icon() . "\n";
-								}
-
-								if ($mycommit->forbidden) {
-									$HTML .= ' ' . freshports_Forbidden_Icon() . "\n";
-								}
-
-								if ($mycommit->broken) {
-									$HTML .= ' '. freshports_Broken_Icon() . "\n";
-								}
-
-								$HTML .= freshports_CommitFilesLink($mycommit->message_id, $mycommit->category, $mycommit->port);
-								$HTML .= "&nbsp;";
-
-							} else {
-#echo 'no category found!<br>';
-								$HTML .= '<BIG><B>';
-								$PathName = preg_replace('|^/?ports/|', '', $mycommit->element_pathname);
-								$HTML .= '<a href="/' . $PathName . '">' . $PathName . '</a>';
-								$HTML .= "</B></BIG>\n";
-							}
-							$HTML .= htmlspecialchars($mycommit->short_description) . "\n";
-						}
-
-						$j++;
-					} // end while
-
-
-					if ($NumberOfPortsInThisCommit > $MaxNumberPortsToShow) {
-						$HTML .= '<BR>' . freshports_MorePortsToShow($mycommit->message_id, $NumberOfPortsInThisCommit, $MaxNumberPortsToShow);
-					}
-					$i = $j - 1;
-
-					$HTML .= "\n<BLOCKQUOTE>";
-#echo "freshports_PortDescriptionPrint called $i<br>";
-
-					$HTML .= freshports_PortDescriptionPrint($mycommit->commit_description, $mycommit->encoding_losses, $freshports_CommitMsgMaxNumOfLinesToShow, freshports_MoreCommitMsgToShow($mycommit->message_id, $freshports_CommitMsgMaxNumOfLinesToShow));
-
-					$HTML .= "\n</BLOCKQUOTE>\n</TD></TR>\n\n\n";
-
-					echo $HTML;
-				}
-
-	           echo "</TABLE>\n";
-			} else {
-				echo "<P>Sorry, nothing found in the database....</P>\n";
-			}
-         } else {
-            echo "read from test failed";
-         }
-      } else {
-         echo "no connection";
-      }
+}
 
 ?>
+</TABLE>
 </TD>
   <TD VALIGN="top" WIDTH="*" ALIGN="center">
    <? freshports_SideBar(); ?>

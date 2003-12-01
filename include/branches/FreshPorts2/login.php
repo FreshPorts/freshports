d50750023cab05ac04eb11601d1cdb950b4962d5
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2003-12-01 18:17:47 +0000

    Use $UserID instead of $User->id
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@2610 46e82423-29d8-e211-989e-002590a4cdd4

--- include/branches/FreshPorts2/login.php
+++ include/branches/FreshPorts2/login.php
@@ -1,6 +1,6 @@
 <?php
 	#
-	# $Id: login.php,v 1.1.2.7 2003-07-04 14:59:19 dan Exp $
+	# $Id: login.php,v 1.1.2.8 2003-12-01 18:17:47 dan Exp $
 	#
 	# Copyright (c) 1998-2003 DVL Software Limited
 	#
@@ -11,7 +11,7 @@
 <form action="<?php echo $_SERVER["PHP_SELF"] . "?origin=$origin" ?>" method="POST" name="l">
       <input type="hidden" name="custom_settings" value="1"><input type="hidden" name="LOGIN" value="1">
       <p>User ID:<br>
-      <input SIZE="15" NAME="UserID" value="<?php if (IsSet($User->id)) echo $User->id ?>"></p>
+      <input SIZE="15" NAME="UserID" value="<?php if (IsSet($UserID)) echo $UserID ?>"></p>
       <p>Password:<br>
       <input TYPE="PASSWORD" NAME="Password" VALUE = "<?php if (IsSet($Password)) echo $Password ?>" size="20"></p>
       <p><input TYPE="submit" VALUE="Login" name=submit> &nbsp;&nbsp;&nbsp;&nbsp; <input TYPE="reset" VALUE="reset form">

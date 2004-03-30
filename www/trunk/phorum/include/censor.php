75746433cdccb2563e27c3ba8a413853eb8872c3
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2004-03-30 20:16:33 +0000

    Remove this form our tree.
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@2790 46e82423-29d8-e211-989e-002590a4cdd4

--- www/trunk/phorum/include/censor.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-  // to use this file add a line like this:
-  // $profan[]="badword";
-  $profan[]="fuck";
-  $profan[]="shit";
-  $profan[]="motherfucker";
-  $profan[]="asshole";
-  $profan[]="son of a bitch";
-?>

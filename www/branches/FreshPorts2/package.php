a5db1415085ff35656c74b7ffdaa0b937040f066
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2004-08-23 19:05:16 +0000

    Page for listing package lookup failures
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@2880 46e82423-29d8-e211-989e-002590a4cdd4

---
+++ www/branches/FreshPorts2/package.php
@@ -0,0 +1,89 @@
+<?php
+	#
+	# $Id: package.php,v 1.1.2.1 2004-08-23 19:05:16 dan Exp $
+	#
+	# Copyright (c) 2004 DVL Software Limited
+	#
+
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/common.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/freshports.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/databaselogin.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/getvalues.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/../classes/searches.php');
+
+	$Title = 'Search by package';
+
+	freshports_Start("$Title",
+					"freshports - new ports, applications",
+					"FreeBSD, index, applications, ports");
+
+	if (IsSet($_REQUEST['notfound'])) $notfound = 1;
+	if (IsSet($_REQUEST['multiple'])) $multiple = 1;
+
+	$package  = AddSlashes($_REQUEST['package']);
+
+	$Searches = new Searches($dbh);
+
+
+?>
+<TABLE WIDTH="<? echo $TableWidth; ?>" BORDER="0" ALIGN="center">
+<TR><td VALIGN=TOP width="100%">
+<TABLE WIDTH="100%" ALIGN="left" border="0">
+<TR>
+	<? echo freshports_PageBannerText($Title); ?>
+</TR>
+
+<TR><TD>
+<P>
+
+<?php
+if ($notfound) {
+?>
+The package specified ('<?php echo $package; ?>') could not be found.  We have a few suggestions.
+<ul>
+<li><a href="<?php echo $Searches->GetDefaultSearchString($package); ?>">Search</a> for ports containing '<?php echo $package; ?>' in their name.
+<li><a href="<?php echo $Searches->GetDefaultSoundsLikeString($package); ?>">Search</a> for ports which sound like '<?php echo $package; ?>'.
+</ul>
+<?php
+} else {
+	if ($multiple) {
+?>
+There is more than one port with the package name '<?php echo $package; ?>'.
+
+<ul>
+<li><a href="<?php echo $Searches->GetDefaultSearchString($package); ?>">Search</a> for ports containing which have '<?php echo $package; ?>' in their name.
+<li><a href="<?php echo $Searches->GetDefaultSoundsLikeString($package); ?>">Search</a> for ports which sound like '<?php echo $package; ?>'.
+</ul>
+<?php
+	} else {
+		die('I have no idea what I should be doing');
+	}
+}
+?>
+</P>
+</TD></TR>
+
+</TABLE>
+</TD>
+
+  <TD VALIGN="top" WIDTH="*" ALIGN="center">
+	<?
+	freshports_SideBar();
+	?>
+  </td>
+
+</TABLE>
+
+<?php
+	GLOBAL $ShowPoweredBy;
+	$ShowPoweredBy = 1;
+?>
+
+<TABLE WIDTH="<? echo $TableWidth; ?>" BORDER="0" ALIGN="center">
+<TR><TD>
+<? freshports_ShowFooter(); ?>
+</TD></TR>
+</TABLE>
+
+</BODY>
+</HTML>

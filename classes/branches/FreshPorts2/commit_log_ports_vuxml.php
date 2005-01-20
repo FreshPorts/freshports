5a45b38d51a54da4cb8e3a7e4b1eb25d4b22cd5e
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2005-01-20 00:07:20 +0000

    Use Unset on the array, so we get the proper values.
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@3207 46e82423-29d8-e211-989e-002590a4cdd4

--- classes/branches/FreshPorts2/commit_log_ports_vuxml.php
+++ classes/branches/FreshPorts2/commit_log_ports_vuxml.php
@@ -1,5 +1,5 @@
 <?php
-	# $Id: commit_log_ports_vuxml.php,v 1.1.2.5 2004-12-19 16:40:29 dan Exp $
+	# $Id: commit_log_ports_vuxml.php,v 1.1.2.6 2005-01-20 00:07:20 dan Exp $
 	#
 	# Copyright (c) 1998-2003 DVL Software Limited
 	#
@@ -67,7 +67,7 @@ select CLPV.id,
 	}
 
 	function VuXML_List_Get($port_id) {
-		$VID = '';
+		unset($VID);
 
 		$numrows = $this->FetchInitialise($port_id);
 		for ($i = 0; $i < $numrows; $i++) {

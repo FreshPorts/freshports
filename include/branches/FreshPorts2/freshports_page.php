111f3d2d4f171660c6ef762db4b322f7ba432075
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2005-01-23 00:22:14 +0000

    Add a FreshPorts page that uses PEAR's HTML/Page2 class.  Very nifty.
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@3222 46e82423-29d8-e211-989e-002590a4cdd4

---
+++ include/branches/FreshPorts2/freshports_page.php
@@ -0,0 +1,94 @@
+<?php
+
+	set_include_path('/usr/local/share/pear');
+	require_once("HTML/Page2.php");
+
+class freshports_page extends HTML_Page2 {
+
+	var $_ShowAds           = 1;
+	var $_BannerAd          = 1;
+	var $_ShowAnnouncements = 0;
+	var $_ShowLogo          = 1;
+
+	var $_db;
+
+    function freshports_page($attributes = array()) {
+		$this->assignDefaultAttributes($attributes);
+
+		$this->HTML_Page2($attributes);
+
+		$this->setMetaData('author',           'Dan Langille');
+		$this->setMetaData('description',      'FreshPorts - new ports, applications');
+		$this->setMetaData('keywords',         'FreeBSD, index, applications, ports');
+
+		$this->setMetaData('Pragma',           'no-cache', TRUE);
+		$this->setMetaData('Cache-Control',    'no-cache', TRUE);
+		$this->setMetaData('Pragma-directive', 'no-cache', TRUE);
+		$this->setMetaData('cache-directive',  'no-cache', TRUE);
+		$this->setMetaData('Expires',          '0',        TRUE);
+
+		$this->setMetaData('robots', 'noarchive');
+		$this->setMetaData('robots', 'noindex');
+
+		$this->addStyleSheet('/css/freshports.css');
+
+		$this->setBodyAttributes(array('BGCOLOR' => '#FFFFFF', 'TEXT' => '#000000'));
+	}
+
+
+	function toHTML() {
+		$HTML = '';
+
+		if ($this->_ShowAds && $this->_BannerAd) {
+			$HTML .= "\n<CENTER>\n" .
+			         BurstMediaAd() .
+			         "</CENTER>\n\n";
+		}
+
+		if ($this->_ShowLogo) {
+			$HTML .= freshports_Logo();
+		}
+
+		if ($this->_ShowAnnouncements) {
+	
+			$Announcement = new Announcement($this->$_db);
+
+			$NumRows = $Announcement->FetchAllActive();
+			if ($NumRows > 0) {
+				$HTML .= DisplayAnnouncements($Announcement);
+			}
+		}
+
+		$HTML .= freshports_MainTable() . "\n<tr><td width='100%' valign='top'>\n" .
+		         freshports_MainContentTable() . "\n<tr>\n" .
+		         freshports_PageBannerText($this->_title) . "\n<tr><td>\n";
+
+		$this->prependBodyContent($HTML);
+
+		$this->addBodyContent("\n</td></tr></table><td>" . freshports_SideBar() . "</td></tr></table>\n" . freshports_ShowFooter());
+
+		return parent::toHTML();
+	}
+
+	function assignDefaultAttributes(&$attributes) {
+		# if no value, give it a default value
+		if (!$attributes || !is_array($attributes)) {
+
+			$attributes['doctype'] = 'HTML 4.01 Transitional';
+		}
+
+		if (!IsSet($attributes['doctype'])) {
+			$attributes['doctype'] = 'HTML 4.01 Transitional';
+		}
+
+		if (!IsSet($attributes['cache'])) {
+			$attributes['cache'] = 'false';
+		}
+
+		if (!IsSet($attributes['prolog'])) {
+			$attributes['prolog'] = 'false';
+		}
+
+	}
+
+}

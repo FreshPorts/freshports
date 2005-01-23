32ba870ec87c932dd6af2a037d712655d9f6402e
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2005-01-23 03:41:33 +0000

    Use the description, remove debug reference.
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@3232 46e82423-29d8-e211-989e-002590a4cdd4

--- www/branches/FreshPorts2/ports-broken.php
+++ www/branches/FreshPorts2/ports-broken.php
@@ -1,6 +1,6 @@
 <?php
 	#
-	# $Id: ports-broken.php,v 1.1.2.19 2005-01-23 03:12:54 dan Exp $
+	# $Id: ports-broken.php,v 1.1.2.20 2005-01-23 03:41:33 dan Exp $
 	#
 	# Copyright (c) 1998-2004 DVL Software Limited
 	#
@@ -12,15 +12,13 @@
 
 	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/freshports_page_list_ports.php');
 
-	$Debug = 0;
-
-	$Title = 'Broken ports';
-
 	$page = new freshports_page_list_ports();
 
 	$page->setDB($db);
 
-	$page->setTitle($Title);
+	$page->setTitle('Broken ports');
+	$page->setDescription('These are the broken ports');
+
 	$page->setSQL("ports.broken <> ''", $User->id);
 
 	$page->display();

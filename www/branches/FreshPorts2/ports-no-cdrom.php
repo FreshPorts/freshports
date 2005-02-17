f1de7836997d321c85624cd461bf3b7ec0276037
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2005-02-17 04:24:24 +0000

    Add two more lists of ports by characteristics.
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@3288 46e82423-29d8-e211-989e-002590a4cdd4

---
+++ www/branches/FreshPorts2/ports-no-cdrom.php
@@ -0,0 +1,25 @@
+<?php
+	#
+	# $Id: ports-no-cdrom.php,v 1.1.2.1 2005-02-17 04:24:24 dan Exp $
+	#
+	# Copyright (c) 1998-2005 DVL Software Limited
+	#
+
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/common.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/freshports.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/databaselogin.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/getvalues.php');
+
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/freshports_page_list_ports.php');
+
+	$page = new freshports_page_list_ports();
+
+	$page->setDB($db);
+
+	$page->setTitle('NO CDROM ports');
+	$page->setDescription('These are the NO CDROM ports');
+
+	$page->setSQL("ports.no_cdrom <> ''", $User->id);
+
+	$page->display();
+?>

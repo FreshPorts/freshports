4a6b03a98d1f56687f06ac1b27a0e940f882a49d
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2005-03-18 02:16:26 +0000

    Add a list of the vulnerable ports
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@3315 46e82423-29d8-e211-989e-002590a4cdd4

---
+++ www/branches/FreshPorts2/ports-vulnerable.php
@@ -0,0 +1,26 @@
+<?php
+	#
+	# $Id: ports-vulnerable.php,v 1.1.2.1 2005-03-18 02:16:26 dan Exp $
+	#
+	# Copyright (c) 1998-2005 DVL Software Limited
+	#
+
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/common.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/freshports.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/databaselogin.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/getvalues.php');
+
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/freshports_page_list_ports.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/freshports_page_list_ports_vulnerable.php');
+
+	$page = new freshports_page_list_ports();
+
+	$page->setDB($db);
+
+	$page->setTitle('Vulnerable ports');
+	$page->setDescription('These are the vulnerable ports');
+
+	$page->setSQL("PV.current != 0", $User->id);
+
+	$page->display();
+?>

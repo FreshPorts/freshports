b5285610f0a6c4a5c9469507c683c27a4e2fd452
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2006-06-22 15:09:46 +0000

    Improve the logging messages by using CachePort not CachPort. Move to a directory structure that reflects the ports tree: i.e. ports/CATEGORYNAME/PORTNAME Introduce and used _CacheFileName()
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@3567 46e82423-29d8-e211-989e-002590a4cdd4

--- classes/branches/FreshPorts2/cache-port.php
+++ classes/branches/FreshPorts2/cache-port.php
@@ -1,6 +1,6 @@
 <?php
 	#
-	# $Id: cache-port.php,v 1.1.2.2 2006-05-29 06:27:19 dan Exp $
+	# $Id: cache-port.php,v 1.1.2.3 2006-06-22 15:09:46 dan Exp $
 	#
 	# Copyright (c) 2006 DVL Software Limited
 	#
@@ -15,7 +15,7 @@ class CachePort extends Cache {
 	}
 	
 	function Retrieve($Category, $Port, &$data) {
-		$this->_Log("CachPort: Retrieving for $Category/$Port");
+		$this->_Log("CachePort: Retrieving for $Category/$Port");
 		$Key = $this->_PortKey($Category, $Port);
 		$result = Parent::Retrieve($Key, $data);
 
@@ -23,15 +23,23 @@ class CachePort extends Cache {
 	}
 
 	function Add($Category, $Port, $data) {
-		$this->_Log("CachPort: Adding for $Category/$Port");
+		$this->_Log("CachePort: Adding for $Category/$Port");
+
+		$CategoryCacheDir = $this->CacheDir . '/ports/' . $Category;
 		$Key = $this->_PortKey($Category, $Port);
+		 
+		if (!file_exists($CategoryCacheDir)) {
+			$this->_Log("CachePort: creating directory $CategoryCacheDir");
+			mkdir($CategoryCacheDir, 0774);
+		}
+		$result = 0;
 		$result = Parent::Add($Key, $data);
 
 		return $result;
 	}
 
 	function Remove($Category, $Port) {
-		$this->_Log("CachPort: Removing for $Category/$Port");
+		$this->_Log("CachePort: Removing for $Category/$Port");
 		$Key = $this->_PortKey($Category, $Port);
 		$result = Parent::Remove($Key, $data);
 
@@ -40,8 +48,14 @@ class CachePort extends Cache {
 	
 	function _PortKey($Category, $Port) {
 		// might want some parameter checking here
-		$Key = $Category . '.' . $Port;
+		$Key = 'ports/' . $Category . '/' . $Port . '.html';
 
 		return $Key;
 	}
+
+	function _CacheFileName($key) {
+		return $this->CacheDir . '/'. $key;
+	}
+	
+
 }

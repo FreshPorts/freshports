a50f8a420250b1d919a81575bd8844ee15fd75ba
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2006-07-01 17:46:42 +0000

    Add a new ad
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@3604 46e82423-29d8-e211-989e-002590a4cdd4

--- include/branches/FreshPorts2/adsense.php
+++ include/branches/FreshPorts2/adsense.php
@@ -1,12 +1,33 @@
 <?php
 	#
-	# $Id: adsense.php,v 1.1.2.2 2006-06-26 12:13:45 dan Exp $
+	# $Id: adsense.php,v 1.1.2.3 2006-07-01 17:46:42 dan Exp $
 	#
 	# Copyright (c) 1998-2006 DVL Software Limited
 	#
 
 	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/constants.php');
 
+function AdSense468x60() {
+
+return '
+<script type="text/javascript"><!--
+google_ad_client = "pub-0711826105743221";
+google_ad_width = 468;
+google_ad_height = 60;
+google_ad_format = "468x60_as";
+google_ad_type = "text_image";
+google_ad_channel ="";
+google_color_border = "2D5893";
+google_color_bg = "99AACC";
+google_color_link = "000000";
+google_color_url = "000099";
+google_color_text = "003366";
+//--></script>
+<script type="text/javascript"
+  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+';
+}
 
 function AdSense728x90() {
 

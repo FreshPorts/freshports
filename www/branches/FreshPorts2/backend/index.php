ff5a71485aff046aa1b81c8d51c28e5ca9bdd06c
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2006-07-14 13:42:42 +0000

    Add a better news service
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@3633 46e82423-29d8-e211-989e-002590a4cdd4

---
+++ www/branches/FreshPorts2/backend/index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title>FreshPorts News Feeds</title>
+</head>
+<body>
+<h1>FreshPorts News Feeds</h1>
+
+<p>
+We have a number of newsfeeds for your consumption:
+
+<ol>
+<li><a href="atom0.3.php">ATOM 0.3</a>
+<li><a href="html.php">HTML</a>
+<li><a href="js.php">Javascript</a>
+<li><a href="mbox.php">mbox</a>
+<li><a href="opml.php">opml</a>
+<li><a href="pie0.1.php">PIE 0.1</a>
+<li><a href="rss0.91.php">RSS 0.91</a>
+<li><a href="rss1.0.php">RSS 1.0</a>
+<li><a href="rss2.0.php">RSS 2.0</a>
+</ol>
+
+<p>
+The above feeds are created using <a href="http://www.bitfolge.de/rsscreator-en.html">FeedCreator</a>.  I hope
+to add Atom 1.0 to FeedCreator. 
+
+<p>
+NOTE: The above feeds may be static for a while.  I've not done anything to refresh the cache.  I'm doing that 
+outside the mechanism provided by FeedCreator, which is quite good by the way.
+
+</body>
+</html>
---
+++ www/branches/FreshPorts2/backend/news.php
@@ -0,0 +1,19 @@
+<?php
+	#
+	# $Id: news.php,v 1.1.2.1 2006-07-14 13:42:42 dan Exp $
+	#
+	# Copyright (c) 1998-2006 DVL Software Limited
+	#
+
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/common.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/freshports.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/databaselogin.php');
+
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/getvalues.php');
+
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/../classes/newsfeed.php'); 
+
+	$format = basename($_SERVER['PHP_SELF'], '.php');
+
+	echo newsfeed($db, $format);
+?>

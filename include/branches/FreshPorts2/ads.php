33504d69363c747948091c62243d191304b018f9
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2006-07-18 02:56:34 +0000

    Try going direct with google.  Does this affect the stats?
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@3652 46e82423-29d8-e211-989e-002590a4cdd4

--- include/branches/FreshPorts2/ads.php
+++ include/branches/FreshPorts2/ads.php
@@ -1,6 +1,6 @@
 <?php
 	#
-	# $Id: ads.php,v 1.1.2.5 2006-07-02 21:26:03 dan Exp $
+	# $Id: ads.php,v 1.1.2.6 2006-07-18 02:56:34 dan Exp $
 	#
 	# Copyright (c) 1998-2006 DVL Software Limited
 	#
@@ -43,15 +43,65 @@ function Ad_125x125() {
 }
 
 function Ad_468x60() {
-  return Ad_PhpPgAdsBase(44, 'addffeb2');
+#  return Ad_PhpPgAdsBase(44, 'addffeb2');
+return '<script type="text/javascript"><!--
+google_ad_client = "pub-0711826105743221";
+google_alternate_ad_url = "http://ads.unixathome.org/AdSense/defaults/728x90.html";
+google_ad_width = 728;
+google_ad_height = 90;
+google_ad_format = "728x90_as";
+google_ad_type = "text_image";
+google_ad_channel ="";
+google_color_border = "2D5893";
+google_color_bg = "99AACC";
+google_color_link = "000000";
+google_color_url = "000099";
+google_color_text = "003366";
+//--></script>
+<script type="text/javascript"
+  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>';
 }
 
 function Ad_728x90() {
-  return Ad_PhpPgAdsBase(42, 'a6a018dd');
+#  return Ad_PhpPgAdsBase(42, 'a6a018dd');
+  return '<script type="text/javascript"><!--
+  google_ad_client = "pub-0711826105743221";
+  google_alternate_ad_url = "http://ads.unixathome.org/AdSense/defaults/728x90.html";
+  google_ad_width = 728;
+  google_ad_height = 90;
+  google_ad_format = "728x90_as";
+  google_ad_type = "text_image";
+  google_ad_channel ="";
+  google_color_border = "2D5893";
+  google_color_bg = "99AACC";
+  google_color_link = "000000";
+  google_color_url = "000099";
+  google_color_text = "003366";
+  //--></script>
+  <script type="text/javascript"
+    src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+    </script>';
 }
 
 function Ad_728x90PortDescription() {
-  return Ad_PhpPgAdsBase(49, 'a88ac84a');
+return '<script type="text/javascript"><!--
+google_ad_client = "pub-0711826105743221";
+google_alternate_ad_url = "http://ads.unixathome.org/AdSense/defaults/728x90.html";
+google_ad_width = 728;
+google_ad_height = 90;
+google_ad_format = "728x90_as";
+google_ad_type = "text_image";
+google_ad_channel ="5402590257";
+google_color_border = "2D5893";
+google_color_bg = "99AACC";
+google_color_link = "000000";
+google_color_url = "000099";
+google_color_text = "003366";
+//--></script>
+<script type="text/javascript"
+  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>';
 }
 
 function Ad_728x90PhorumBottom() {

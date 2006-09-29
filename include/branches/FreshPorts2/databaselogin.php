80e795a6c6ad261f8071a8d245c21c6555918a98
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2006-09-29 12:53:35 +0000

    Log some URLs so we can find out which one is killing the httpd process.
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@3802 46e82423-29d8-e211-989e-002590a4cdd4

--- include/branches/FreshPorts2/databaselogin.php
+++ include/branches/FreshPorts2/databaselogin.php
@@ -1,6 +1,6 @@
 <?php
 	#
-	# $Id: databaselogin.php,v 1.1.2.4 2003-09-30 11:37:48 dan Exp $
+	# $Id: databaselogin.php,v 1.1.2.5 2006-09-29 12:53:35 dan Exp $
 	#
 	# Copyright (c) 1998-2003 DVL Software Limited
 	#
@@ -8,6 +8,9 @@
 	# this file is merely a placeholder for the readfile.
 	# see common.php for the configuration settings.
 
+openlog('FRESHPORTS PID', LOG_PID | LOG_NDELAY, LOG_SYSLOG);
+syslog(LOG_NOTICE, $_SERVER["REQUEST_URI"]);
+closelog();
 	require_once("$PathToDatabaseConfigFile/database.php");
 
 	if (!$AllowUserChanges) {

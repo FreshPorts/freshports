d02da8408fd3c79dc744212219441fcfb77fa769
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2006-12-02 16:37:09 +0000

    Add an ad
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@3988 46e82423-29d8-e211-989e-002590a4cdd4

--- configuration/branches/FreshPorts2/phorum/footer.php
+++ configuration/branches/FreshPorts2/phorum/footer.php
@@ -1,6 +1,17 @@
 
 </TABLE>
 
+<br>
+
+<?php
+  if ($BannerAd) {
+    echo "\n<CENTER>\n";
+    echo Ad_728x90PhorumTop();
+    echo "</CENTER>\n\n";
+    }
+?>
+
+
 <TABLE WIDTH="<? echo $TableWidth; ?>" BORDER="0" ALIGN="center">
 <TR><TD>
 <? echo freshports_ShowFooter(1); ?>

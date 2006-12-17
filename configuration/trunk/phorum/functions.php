99892539016485cff5050628fe375b69cbda33a9
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2006-12-17 11:41:26 +0000

    Merge from FreshPorts2 to HEAD
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@4010 46e82423-29d8-e211-989e-002590a4cdd4

---
+++ configuration/trunk/phorum/functions.php
@@ -0,0 +1,12 @@
+<?php
+	#
+	# $Id: functions.php,v 1.2 2006-12-17 11:39:37 dan Exp $
+	#
+	# Copyright (c) 1998-2004 DVL Software Limited
+	#
+
+function custom_SearchPathForum() {
+	return '';
+}
+
+?>

c0fcf5616459e573cd14a21021e0de579bd160fd
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2007-04-02 03:21:19 +0000

    Add
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@4080 46e82423-29d8-e211-989e-002590a4cdd4

---
+++ www/trunk/DesignContestVoting/index.php
@@ -0,0 +1,47 @@
+<html>
+<body>
+<h1>FreshPorts Design Contest</h1>
+
+<p>
+Back in January, I announced the <a href="http://news.freshports.org/2007/01/30/freshports-design-contest/">FreshPorts design contest</a>.  The deadline has passed,
+and the results appear below.  We have three options.  A and B are submitted designs.
+Option C is what we have now.
+
+<p>
+I will be asking for votes.  Votes will be accepted only from logged in FreshPorts 
+users with accounts created before today.  No, you can't create a bunch of new accounts
+now and load the voting.
+
+<p>
+Voting will be open for two weeks.
+
+<p>
+
+<h2>Option A</h2>
+
+<ul>
+<li><a href="A/index.html">home page</a></li>
+<li><a href="A/bacula.html">sample port</a></li>
+<li><a href="A/sysutils.html">sample category</a></li>
+<li><a href="A/categories.html">all categories</a></li>
+<li><a href="A/search.html">search page</a></li>
+</ul>
+
+
+<h2>Option B</h2>
+<ul>
+<li><a href="B/index.html">home page</a></li>
+</ul>
+
+<h2>Option C</h2>
+
+<ul>
+<li><a href="http://www.freshports.org/">home page</a></li>
+<li><a href="http://www.freshports.org/sysutils/bacula-server/">sample port</a></li>
+<li><a href="http://www.freshports.org/sysutils/">sample category</a></li>
+<li><a href="http://www.freshports.org/categories.php">all categories</a></li>
+<li><a href="http://www.freshports.org/search.php">search page</a></li>
+</ul>
+
+</body>
+</html>

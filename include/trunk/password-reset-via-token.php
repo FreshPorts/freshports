e9da52fe49b5557eaaadac4aec8f324b10291460
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2010-09-17 14:44:55 +0000

    Add form for forgotten passwords
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@4328 46e82423-29d8-e211-989e-002590a4cdd4

---
+++ include/trunk/password-reset-via-token.php
@@ -0,0 +1,28 @@
+<?php
+	#
+	# $Id: password-reset-via-token.php,v 1.1 2010-09-17 14:44:55 dan Exp $
+	#
+	# Copyright (c) 1998-2003 DVL Software Limited
+	#
+
+	if (IsSet($_REQUEST["origin"])) $origin = $_REQUEST["origin"];
+	GLOBAL $numberofdays;
+	GLOBAL $page_size;
+?>
+
+<form action="<?php echo $_SERVER["PHP_SELF"] . "?origin=" . $origin ?>" method="POST" NAME=f>
+<TABLE width="*" border="0" cellpadding="1">
+          <TR>
+            <TD VALIGN="top">
+              <INPUT TYPE="hidden" NAME="token" VALUE="<?php echo $token ?>">
+               Password:<br>
+               <INPUT TYPE="PASSWORD" NAME="Password1" VALUE="<?php if (IsSet($Password1)) echo htmlentities($Password1) ?>" size="20"><br><br>
+               Confirm Password:<br>
+               <INPUT TYPE="PASSWORD" NAME="Password2" VALUE="<?php if (IsSet($Password2)) echo htmlentities($Password2) ?>" size="20">
+               <br><br>
+            <INPUT TYPE="submit" VALUE="Set password" NAME="submit">
+            </TD>
+          </TR>
+</TABLE>
+</FORM>
+

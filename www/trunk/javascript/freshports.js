4588d4ee8f3bb1b42988c8dd277cc3cedcfd4ed2
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2011-02-07 00:46:01 +0000

    Add
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@4360 46e82423-29d8-e211-989e-002590a4cdd4

---
+++ www/trunk/javascript/freshports.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  var clickFnExpand = function() {
+    var ctl = $($(this).attr('data-control'));
+
+    ctl.toggle()
+
+    var hidden = ctl.is(':hidden');
+
+    $(this).attr('src',   hidden ? '/images/expand.gif' : '/images/contract.gif' );
+    $(this).attr('alt',   hidden ? 'Expand depends'     : 'Contract depends' );
+    $(this).attr('title', hidden ? 'Expand depends'     : 'Contract depends' );
+    return false;
+  };
+
+  $('.contract').click( clickFnExpand );
+  $('.contract').click();
+  
+});
+                                

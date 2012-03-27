fa9321d75d6e9c3fbe7dbee47dc4c93b9f3a77f5
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2012-03-27 19:50:50 +0000

    Block images
    git-svn-id: svn+ssh://svn.int.unixathome.org/usr/local/svn/repos/freshports-1@4394 46e82423-29d8-e211-989e-002590a4cdd4

--- configuration/trunk/phorum/functions.php
+++ configuration/trunk/phorum/functions.php
@@ -1,6 +1,6 @@
 <?php
 	#
-	# $Id: functions.php,v 1.3 2012-03-14 15:51:45 dan Exp $
+	# $Id: functions.php,v 1.4 2012-03-27 19:50:50 dan Exp $
 	#
 	# Copyright (c) 1998-2004 DVL Software Limited
 	#
@@ -9,5 +9,6 @@ function custom_SearchPathForum() {
 	return '';
 }
 	define('NO_WEBPAGE', true);
+	define('NO_IMAGE',   true);
 
 ?>

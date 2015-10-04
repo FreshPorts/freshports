388e6ddb56dcde76fe92d2fc6e5a0e4942bc211b
Dan Langille <dan@langille.org>
2015-10-04 10:14:35 +0000

    If the URL is not parsed above, raise a 404

--- www/--/index.php
+++ www/--/index.php
@@ -5,7 +5,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/../include/freshports.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/../include/databaselogin.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/../include/getvalues.php');
 
-require_once($_SERVER['DOCUMENT_ROOT'] . '/--/functions.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/../rewrite/functions.php');
 
 
 #echo phpinfo();
@@ -17,7 +17,9 @@ $url = parse_url($query);
 
 parse_str($query, $url_parts);
 
+$Debug = 0;
 if ($Debug) {
+#	phpinfo();
     echo '<pre>';
 
     echo 'script = ';
@@ -33,11 +35,14 @@ if ($Debug) {
     var_dump($url_parts);
 }
 
-$result = freshports_Parse404URI($_SERVER['REDIRECT_URL'], $db);
+$result = freshports_Parse404URI($_SERVER['REQUEST_URI'], $db);
 
 # if you get here, we could not find anything in the database, so let's run 
 # the 404 code.
 #
 # XXX move missing.php out of DOCUMENT_ROOT
+#echo "\$result='$result'";
 
-require_once($_SERVER['DOCUMENT_ROOT'] . '/missing.php');
+if ($result != '') {
+	require($_SERVER['DOCUMENT_ROOT'] . '/../rewrite/missing.php');
+}

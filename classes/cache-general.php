45ce60636609808551fa8793dfaa93ae6d8b08b5
Dan Langille <dan@langille.org>
2017-12-03 16:54:37 +0000

    A caching approach for things which are not in the ports tree.
    * cache-file.php    - for stuff in the ports tree but not a port (e.g. sysutils/bacula-server/Makefile)
    * cache-general.php - things not in the ports tree (e.g. sanity_test_failures.php)
    * cache-port.php    - for ports (e.g. sysutils/bacula-server)
    * cache.php         - underlying class used by all of the above

---
+++ classes/cache-general.php
@@ -0,0 +1,80 @@
+<?php
+	#
+	# $Id: cache-file.php,v 1.1 2007-06-03 16:00:17 dan Exp $
+	#
+	# Copyright (c) 2006-2007 DVL Software Limited
+	#
+
+	require_once('cache.php');
+
+// base class for caching general stuff (neither ports, nor files under ports).
+
+class CacheGeneral extends Cache {
+
+	const CacheCategory = 'general';
+
+	var $PageSize = 100;
+
+	function CacheFile() {
+		return parent::Cache();
+	}
+	
+	function PageSizeSet($PageSize) {
+		$this->PageSize = $PageSize;
+	}
+
+	function Retrieve($FileName, $PageNum = 1) {
+		$this->_Log("CacheFile: Retrieving for $FileName");
+		$Key = $this->_FileKey($FileName, $PageNum);
+		$result = parent::Retrieve($Key);
+
+		return $result;
+	}
+
+	function Add($FileName, $PageNum = 1) {
+		$this->_Log("CacheFile: Adding for $FileName");
+
+		$CacheDir = $this->CacheDir . '/' . CacheCategory . '/' . dirname($FileName);
+		$Key = $this->_FileKey($FileName, $PageNum);
+		 
+		if (!file_exists($CacheDir)) {
+			$this->_Log("CacheFile: creating directory $CacheDir");
+			$old_mask = umask(0000);
+			if (!mkdir($CacheDir, 0774, true)) {
+				$this->_Log("CacheFile: unable to create directory $CacheDir");
+			}
+			umask($old_mask);
+		}
+
+		$result = 0;
+		$result = parent::Add($Key);
+
+		return $result;
+	}
+
+	function Remove($FileName) {
+		$this->_Log("CacheFile: Removing for $FileName");
+
+		#
+		# the wild card allows us to remove all cache entries for this port
+		# regardless of the CacheType or page number
+		#
+		$Key = $this->_FileKey($FileName, '*');
+		$result = parent::Remove($Key, $data);
+
+		return $result;
+	}
+	
+	function _FileKey($FileName, $PageNum = 1) {
+		// might want some parameter checking here
+		$Key = CacheCategory . "/$FileName.PageSize$this->PageSize.PageNum$PageNum.html";
+
+		return $Key;
+	}
+
+	function _CacheFileName($key) {
+		return $this->CacheDir . '/'. $key;
+	}
+	
+
+}

FreshPorts/freshports

commit 49c05f2304bd93353abcdd22ec18b1e8db70d960

Remove the <hr> from the post-display code and add it to the search results.
fixes #122

www/search.php
@@ -1069,6 +1069,7 @@ switch ($stype) {
 					$HTML .= $port_display->DisplayPlainText() . "\n";
 					break;
 			} // switch
+			$HTML .= '<hr width="100%">';
 		} // for
 
 	    	if ($output_format == OUTPUT_FORMAT_HTML) {

classes/port-display.php
@@ -588,7 +588,7 @@ class port_display {
 
 		# only show if we're meant to show, and if the port has not been deleted.
 		if ($this->ShowPackageLink || $this->ShowEverything) {
-			$HTML .= "\n</dl><hr><dl>\n";
+			$HTML .= "\n</dl><dl>\n";
 			if ($port->IsDeleted()) {
 				$HTML .= '<dt>No installation instructions: this port has been deleted.</dt>';
 				$HTML .= '<dt>The package name of this deleted port was: <code class="code">' . $port->latest_link . '</code></dt>';

commit ffc6260649c452eccf9962376783d628c9c0dcfe

Fix commit 49c05f2 which introduced a regression
That HR should be in the HTML version only.

Spotted by: feld

www/search.php
@@ -1069,7 +1069,9 @@ switch ($stype) {
 					$HTML .= $port_display->DisplayPlainText() . "\n";
 					break;
 			} // switch
-			$HTML .= '<hr width="100%">';
+			if ($output_format == OUTPUT_FORMAT_HTML) {
+				$HTML .= '<hr width="100%">';
+			}
 		} // for
 
 	    	if ($output_format == OUTPUT_FORMAT_HTML) {
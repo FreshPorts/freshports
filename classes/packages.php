95ffea0b69a82b3ac5535ba2f3f65dd21ee1a6a7
Dan Langille <dan@langille.org>
2020-05-17 20:09:03 +0000

    Add back GetCategoryPortFromPackageName()
    This removed via 67f814963f5d9e974fca8ca5aa93c04c9ae69a00
    
    No idea why.

--- classes/packages.php
+++ classes/packages.php
@@ -18,6 +18,27 @@ class Packages {
 		$this->dbh	= $dbh;
 	}
 
+	function GetCategoryPortFromPackageName($package) {
+		$this->package = $package;
+		$sql = "select GetCategoryPortFromLatestLink('" . pg_escape_string($package) . "') as categoryport";
+#		echo "<pre>sql = '$sql'</pre><BR>";
+
+		$result = pg_query($this->dbh, $sql);
+		if ($result) {
+			$numrows = pg_numrows($result);
+			# there should only be one row.
+			if ($numrows == 1) {
+				$myrow = pg_fetch_array ($result, 0);
+				$this->CategoryPort = $myrow['categoryport'];
+			} else {
+				$this->CategoryPort = '';
+			}
+		} else {
+			echo 'Packages SQL failed: ' . $result . pg_last_error();
+		}
+
+        return $this->CategoryPort;
+	}
 	function Fetch($PortID) {
 		# fetch all rows in ports_updating with id = $PortID
 

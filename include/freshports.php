92d488c63fa7fe62935ba71c134c766ef8aa7591
Jethro Nederhof <jethro@jethron.id.au>
2021-04-18 19:37:32 +1000

    Migrate tooltip CSS from inline to CSS file

--- include/freshports.php
+++ include/freshports.php
@@ -965,39 +965,7 @@ function freshports_Header($ArticleTitle, $Description, $Keywords, $Phorum=0) {
 }
 
 function freshports_style($Phorum=0) {
-
 	echo '	<link rel="stylesheet" href="/css/freshports.css" type="text/css">' . "\n";
-
-	# from https://www.w3schools.com/css/css_tooltip.asp
-	# initially planned for Quarterly branch information: https://github.com/FreshPorts/freshports/issues/115
-	echo '<style type="text/css">
-.tooltip {
-  position: relative;
-  display: inline-block;
-  border-bottom: 1px dotted black;
-}
-
-.tooltip .tooltiptext {
-  visibility: hidden;
-  width: 120px;
-  background-color: black;
-  color: #fff;
-  text-align: center;
-  border-radius: 6px;
-  padding: 5px 0;
-  
-  /* Position the tooltip */
-  position: absolute;
-  z-index: 1;
-  top: -5px;
-  left: 105%;
-}
-
-.tooltip:hover .tooltiptext {
-  visibility: visible;
-}
-</style>';
-
 }
 
 function freshports_body($ExtraScript = null) {

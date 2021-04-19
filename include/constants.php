0196edbf284b9d5a07900985ca7cc0d4eded0780
Jethro Nederhof <jethro@jethron.id.au>
2021-04-19 10:24:53 +1000

    Make password hash constants overridable via constants.local

--- include/constants.php
+++ include/constants.php
@@ -198,5 +198,5 @@ const SEARCH_SELECT_FIELD = '
 # www/new-user.php
 # www/customize.php
 const HASH_UPDATE_QUERY = 'HASH_UPDATE';
-const PW_HASH_METHOD = 'bf';
-const PW_HASH_COST = 14;
+if (!defined('PW_HASH_METHOD')) define('PW_HASH_METHOD', 'bf');
+if (!defined('PW_HASH_COST')) define('PW_HASH_COST', 14);

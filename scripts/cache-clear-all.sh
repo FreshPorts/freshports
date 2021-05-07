2da01e2dd5519b782f3efe6ee8814b10b7d75654
Dan Langille <dan@langille.org>
2021-05-07 21:03:15 +0000

    Add script for clearing out zfs cache via rollback
    The top level zpool is defined as fp_zfs_caching_parent in
    configuration/config.sh.sample
    
    This meant to be installed at /usr/local/etc/freshports/config.sh on the web jail.
    
    The script is run via sudo cache-clear-all.sh

--- scripts/cache-clear-all.sh
+++ scripts/cache-clear-all.sh
@@ -3,8 +3,20 @@
 # This scripts clears the cache of all port related data.
 # Typically, this data resides at ~freshports/cache/ports/
 
-cd ~freshports/cache/ports
+CONFIG="/usr/local/etc/freshports/config.sh"
 
-rm -rf *
+if [ ! -f $CONFIG ]
+then
+        echo "$CONFIG not found by $0..."
+        exit 1
+fi
 
-echo deleted.
+. $CONFIG
+
+command="/sbin/zfs list -Hr -o name -d 1 $fp_zfs_caching_parent"
+
+datasets=$(${command} | sed -n -e '2,$p')
+for dataset in $datasets
+do
+  /sbin/zfs rollback $dataset@empty
+done

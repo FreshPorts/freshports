7c48d83f09ce428c1685d8fb214f058d8837f42f
Dan Langille <dan@langille.org>
2022-10-02 13:40:57 +0000

    change the icon separator from &ndash; to &#166;

--- include/constants.php
+++ include/constants.php
@@ -216,4 +216,4 @@ if (!defined('PW_HASH_COST'))   define('PW_HASH_COST', 14);
 
 define('HTML_DOCTYPE', '<!DOCTYPE html>');
 
-define('ICON_SEPARATOR', ' &ndash; ');
+define('ICON_SEPARATOR', ' &#166; ');

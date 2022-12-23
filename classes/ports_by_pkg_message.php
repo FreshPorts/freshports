23edeb102439a94a50b4086e7a6f3d8c111f1101
Dan Langille <dan@langille.org>
2022-12-23 21:53:40 +0000

    Fix searching by pkgmessage
    re: #401

--- classes/ports_by_pkg_message.php
+++ classes/ports_by_pkg_message.php
@@ -21,15 +21,15 @@ class PortsByPkgMessage extends Port {
 	
 	var $LocalResult = null;
 	
-	const WITH_CLAUSE = 'WITH short_list AS MATERIALIZED (
+	const WITH_CLAUSE = "WITH short_list AS MATERIALIZED (
     SELECT
         DISTINCT id as port_id
     FROM
         ports
     WHERE
-        pkgmessage_textsearchable  @@ websearch_to_tsquery($1) OR
-        pkgmessage_textsearchable2 @@ websearch_to_tsquery($1)
-)';
+        pkgmessage @@ websearch_to_tsquery('english', $1) OR
+        pkgmessage @@ websearch_to_tsquery('simple',  $1)
+)";
 
 	function __construct($dbh) {
 		parent::__construct($dbh);

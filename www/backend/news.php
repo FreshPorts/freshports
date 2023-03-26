FreshPorts/freshports

commit 19f521350a32ee1757adfd8e2c19315fe2d84556

Fix more SQL injection situations.
Reported by: Christian Inci - https://github.com/chris-pcguy

www/backend/news.php
@@ -14,7 +14,7 @@
 	$format = basename($_SERVER['PHP_SELF'], '.php');
 
 	if (IsSet($_REQUEST['branch'])) {
-		$BranchName = NormalizeBranch(htmlspecialchars($_REQUEST['branch']));
+		$BranchName = NormalizeBranch(htmlspecialchars(pg_escape_string($db, $_REQUEST['branch'])));
 	} else {
 		$BranchName = BRANCH_HEAD;
 	}
@@ -22,12 +22,8 @@
 	$BranchName = NormalizeBranch($BranchName);
 
 	$Flavor = '';
-	if (IsSet($_REQUEST['flavor'])) {
-		$Flavor = htmlspecialchars($_REQUEST['flavor']);
-	}
-
 	if (IsSet($_REQUEST['flavour'])) {
-		$Flavor = htmlspecialchars($_REQUEST['flavour']);
+		$Flavor = htmlspecialchars(pg_escape_string($db, pg_escape_string($db, $_REQUEST['flavour'])));
 	}
 
 	$OrderBy = '';

www/backend/announcements.php
@@ -16,9 +16,6 @@
 
 	GLOBAL $User;
 
-$submit  = isset($_REQUEST['submit'])        ? $_REQUEST['submit']        : null;
-$visitor = isset($_COOKIE[USER_COOKIE_NAME]) ? $_COOKIE[USER_COOKIE_NAME] : null;
-
 // if we don't know who they are, we'll make sure they login first
 if (!$User->id) {
 	header('Location: /login.php');  /* Redirect browser to PHP web site */
@@ -32,10 +29,10 @@ if (!$User->IsTaskAllowed(FRESHPORTS_TASKS_ANNOUNCEMENTS_MAINTAIN)) {
 if (IsSet($_REQUEST['add'])) {
 	$Announcement = new Announcement($db);
 
-	$Announcement->TextSet     ($_REQUEST['announcement']);
-	$Announcement->TextPlainSet($_REQUEST['announcement_plain']);
-	$Announcement->StartDateSet($_REQUEST['start_date']);
-	$Announcement->EndDateSet  ($_REQUEST['end_date']);
+	$Announcement->TextSet     (pg_escape_string($db, $_REQUEST['announcement']));
+	$Announcement->TextPlainSet(pg_escape_string($db, $_REQUEST['announcement_plain']));
+	$Announcement->StartDateSet(pg_escape_string($db, $_REQUEST['start_date']));
+	$Announcement->EndDateSet  (pg_escape_string($db, $_REQUEST['end_date']));
 
 	$Announcement->Insert();
 	Unset($Announcement);
@@ -44,11 +41,11 @@ if (IsSet($_REQUEST['add'])) {
 if (IsSet($_REQUEST['update'])) {
 	$Announcement = new Announcement($db);
 
-	$Announcement->TextSet     ($_REQUEST['announcement']);
-	$Announcement->TextPlainSet($_REQUEST['announcement_plain']);
-	$Announcement->StartDateSet($_REQUEST['start_date']);
-	$Announcement->EndDateSet  ($_REQUEST['end_date']);
-	$Announcement->IDSet       ($_REQUEST['id']);
+	$Announcement->TextSet     (pg_escape_string($db, $_REQUEST['announcement']));
+	$Announcement->TextPlainSet(pg_escape_string($db, $_REQUEST['announcement_plain']));
+	$Announcement->StartDateSet(pg_escape_string($db, $_REQUEST['start_date']));
+	$Announcement->EndDateSet  (pg_escape_string($db, $_REQUEST['end_date']));
+	$Announcement->IDSet       (pg_escape_string($db, intval($_REQUEST['id'])));
 
 	$Announcement->Update();
 	Unset($Announcement);
@@ -57,7 +54,7 @@ if (IsSet($_REQUEST['update'])) {
 if (IsSet($_REQUEST['delete'])) {
 	$Announcement = new Announcement($db);
 
-	$Announcement->IDSet($_REQUEST['delete']);
+	$Announcement->IDSet(intval(pg_escape_string($db, $_REQUEST['delete'])));
 
 	$Announcement->Delete();
 	Unset($Announcement);
@@ -67,7 +64,7 @@ if (IsSet($_REQUEST['delete'])) {
 $Announcement = new Announcement($db);
 
 if (IsSet($_REQUEST['edit'])) {
-	$Announcement->Fetch($_REQUEST['edit']);
+	$Announcement->Fetch(intval(pg_escape_string($db, $_REQUEST['edit'])));
 }
 
 	#echo '<br>the page size is ' . $page_size . ' : ' . $email;

www/backend/commits-recent.php
@@ -15,7 +15,7 @@
 	$Debug = 0;
 
 	if (IsSet($_REQUEST['n'])) {
-		$MaxCommits = pg_escape_string($db, $_REQUEST['n']);
+		$MaxCommits = intval(pg_escape_string($db, $_REQUEST['n']));
 	}
 	if (IsSet($MaxCommits)) {
 		if ($MaxCommits < 1 or $MaxCommits > MAX_COMMITS) {

www/commit.php
@@ -33,8 +33,8 @@
 		parse_str($url_query, $url_args);
 	}
 
-	$clean['category'] = $url_args['category'] ?? null;
-	$clean['port']     = $url_args['port']     ?? null;
+	$clean['category'] = pg_escape_string($db, $url_args['category']) ?? null;
+	$clean['port']     = pg_escape_string($db, $url_args['port'])     ?? null;
 
 	$FilesForJustOnePort = IsSet($url_args['category']) && IsSet($url_args['port']);
 	$files = isset($url_args['files']) ? $url_args['files'] : 'n';
@@ -106,8 +106,8 @@
 		}
 	}
 
-	if (IsSet($_REQUEST['page']))      $PageNo   = $_REQUEST['page'];
-	if (IsSet($_REQUEST['page_size'])) $PageSize = $_REQUEST['page_size'];
+	if (IsSet($_REQUEST['page']))      $PageNo   = intval($_REQUEST['page']);
+	if (IsSet($_REQUEST['page_size'])) $PageSize = intval($_REQUEST['page_size']);
 
 	if ($Debug) {
 		echo "\$page      = '$page'<br>\n";

www/commits.php
@@ -50,9 +50,9 @@
 	$days         = $NumberOfDays;
 	$dailysummary = 7;
 
-	if (IsSet($_REQUEST['num']))          $num          = pg_escape_string($db, $_REQUEST["num"]);
-	if (IsSet($_REQUEST['dailysummary'])) $dailysummary = pg_escape_string($db, $_REQUEST["dailysummary"]);
-	if (IsSet($_REQUEST['days']))         $days         = pg_escape_string($db, $_REQUEST["days"]);
+	if (IsSet($_REQUEST['num']))          $num          = pg_escape_string($db, intval($_REQUEST["num"]));
+	if (IsSet($_REQUEST['dailysummary'])) $dailysummary = pg_escape_string($db, intval($_REQUEST["dailysummary"]));
+	if (IsSet($_REQUEST['days']))         $days         = pg_escape_string($db, intval($_REQUEST["days"]));
 
 	if (Is_Numeric($num)) {
 		$MaxNumberOfPortsLong = min($MaxNumberOfPortsLong, max(10, $num));
@@ -159,7 +159,7 @@ A port is marked as new for 10 days.
 		$LatestCommits->SetMaxNumberOfPorts($MaxNumberOfPortsLong);
 		$LatestCommits->SetDaysMarkedAsNew ($DaysMarkedAsNew);
 		$LatestCommits->SetUserID($User->id);
-		$LatestCommits->SetBranch($Branch);
+		$LatestCommits->SetBranch(pg_escape_string($db, $Branch));
 		$LatestCommits->SetWatchListAsk($User->watch_list_add_remove);
 		$LatestCommits->CreateHTML();
 

www/customize.php
@@ -40,8 +40,8 @@ if (IsSet($submit)) {
 	$Password      = $_POST['Password'];
 	$Password1     = $_POST['Password1'];
 	$Password2     = $_POST['Password2'];
-	$numberofdays  = pg_escape_string($db, $_POST['numberofdays']);
-	$page_size     = pg_escape_string($db, $_POST['page_size']);
+	$numberofdays  = pg_escape_string($db, intval($_POST['numberofdays']));
+	$page_size     = pg_escape_string($db, intval($_POST['page_size']));
 
 	# this is a checkbox
 	if (IsSet($_POST['set_focus_search'])) {

www/filter-setup.php
@@ -34,7 +34,7 @@
 if (IsSet($_REQUEST['wlid'])) {
 		# they clicked on the GO button and we have to apply the 
 		# watch staging area against the watch list.
-		$wlid = pg_escape_string($db, $_REQUEST["wlid"]);
+		$wlid = pg_escape_string($db, intval($_REQUEST["wlid"]));
 		if ($Debug) echo "setting SetLastWatchListChosen => \$wlid='$wlid'";
 		$User->SetLastWatchListChosen($wlid);
 		if ($Debug) echo "\$wlid='$wlid'";
@@ -62,7 +62,7 @@ if ($submit) {
     // make sure we are pointing at the start of the array.
     reset($categories);
     foreach ($categories as $key => $value) {
-      $result = $WatchListElement->Add($User->id, $wlid, $value);
+      $result = $WatchListElement->Add($User->id, $wlid, intval($value));
 
 #      ${"category_".$value} = 1;
       if ($result != 1) {
@@ -93,7 +93,7 @@ $visitor = $_COOKIE[USER_COOKIE_NAME];
 if ($_REQUEST['wlid']) {
 		# they clicked on the GO button and we have to apply the 
 		# watch staging area against the watch list.
-		$wlid = pg_escape_string($db, $_REQUEST['wlid']);
+		$wlid = pg_escape_string($db, int_val($_REQUEST['wlid']));
 		if ($Debug) echo "setting SetLastWatchListChosen => \$wlid='$wlid'";
 		$User->SetLastWatchListChosen($wlid);
 		if ($Debug) echo "\$wlid='$wlid'";

www/filter.php
@@ -82,9 +82,9 @@ $num          = $MaxNumberOfPortsLong;
 $days         = $NumberOfDays;
 $dailysummary = 7;
 
-if (IsSet($_REQUEST['num']))          $num          = pg_escape_string($db, $_REQUEST["num"]);
-if (IsSet($_REQUEST['dailysummary'])) $dailysummary = pg_escape_string($db, $_REQUEST["dailysummary"]);
-if (IsSet($_REQUEST['days']))         $days         = pg_escape_string($db, $_REQUEST["days"]);
+if (IsSet($_REQUEST['num']))          $num          = pg_escape_string($db, intval($_REQUEST["num"]));
+if (IsSet($_REQUEST['dailysummary'])) $dailysummary = pg_escape_string($db, intval($_REQUEST["dailysummary"]));
+if (IsSet($_REQUEST['days']))         $days         = pg_escape_string($db, intval($_REQUEST["days"]));
 
 if (Is_Numeric($num)) {
 	$MaxNumberOfPortsLong = min($MaxNumberOfPortsLong, max(10, $num));

commit 135d7746deeeece42b94bad39c0d555e727cf71c

Move from flavour to flavor.
This fixes a bug introduced in 19f521350a32ee1757adfd8e2c19315fe2d84556

www/backend/news.php
@@ -22,8 +22,8 @@
 	$BranchName = NormalizeBranch($BranchName);
 
 	$Flavor = '';
-	if (IsSet($_REQUEST['flavour'])) {
-		$Flavor = htmlspecialchars(pg_escape_string($db, pg_escape_string($db, $_REQUEST['flavour'])));
+	if (IsSet($_REQUEST['flavor'])) {
+		$Flavor = htmlspecialchars(pg_escape_string($db, pg_escape_string($db, $_REQUEST['flavor'])));
 	}
 
 	$OrderBy = '';
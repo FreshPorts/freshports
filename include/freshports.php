2600fe85fc83b764c84ee2d4b7a8ebbab7844db5
Dan Langille <dan@langille.org>
2026-03-20 11:53:40 +0000

    Adjust selling points.

--- include/freshports.php
+++ include/freshports.php
@@ -1077,9 +1077,9 @@ GLOBAL $dbhost;
     if (file_exists(HEAD_FILE)) {
 		# taken from https://stackoverflow.com/questions/7447472/how-could-i-display-the-current-git-branch-name-at-the-top-of-the-page-of-my-de
 		#
-		$HTML .= '<span class="branch">The git branch used by this host is <span class="file">' . implode('/', array_slice(explode('/', file_get_contents(HEAD_FILE)), 2)) . '</span></span><br>';
-		$HTML .= '<span class="branch">The database is   <span class="file">' . $dbname . '</span></span><br>';
-		$HTML .= '<span class="branch">The db server is   <span class="file">' . $dbhost . '</span></span><br>';
+		$HTML .= '<span class="branch">The git branch used by this host is <span class="file">' . implode('/', array_slice(explode('/', file_get_contents(HEAD_FILE)), 2)) . '</span><br>';
+		$HTML .= 'The database is   <span class="file">' . $dbname . '</span><br>';
+		$HTML .= 'The db server is   <span class="file">' . $dbhost . '</span></span><br>';
     }
     if (defined('SHOW_ANIMATED_BUG') && SHOW_ANIMATED_BUG)
     {
@@ -1095,8 +1095,8 @@ GLOBAL $dbhost;
 ";
 	}
 
-	$HTML .= '<span class="amazon">As an Amazon Associate I earn from qualifying purchases.<br>Want a good read? Try <a target="_blank" rel="noopener noreferrer" href="https://www.amazon.com/gp/product/B07PVTBWX7/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B07PVTBWX7&amp;linkCode=as2&amp;tag=thfrdi0c-20&amp;linkId=a5cb3ac309f59900d44401e24a169f05">FreeBSD Mastery: Jails (IT Mastery Book 15)</a></span>';
-	$HTML .= '<span class="amazon">Want a good monitor light? See my <a href="https://dan.langille.org/2023/12/30/blatant-self-interest-monitor-light/">photos</a></span>';
+	$HTML .= '<span class="amazon">As an Amazon Associate I earn from qualifying purchases.<br>Want a good read? Try <a target="_blank" rel="noopener noreferrer" href="https://www.amazon.com/gp/product/B07PVTBWX7/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B07PVTBWX7&amp;linkCode=as2&amp;tag=thfrdi0c-20&amp;linkId=a5cb3ac309f59900d44401e24a169f05">FreeBSD Mastery: Jails (IT Mastery Book 15)</a><br>';
+	$HTML .= 'Want a good monitor light? See my <a href="https://dan.langille.org/2023/12/30/blatant-self-interest-monitor-light/">photos</a></span>';
 	$HTML .= '<span class="timezone">All times are UTC</span>';
 
 	$HTML .= '</td>';
